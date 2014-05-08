07cc2c0c929f402bbdb07db57e5df7c96f2675b7
Justin Huang <jstn@cs.washington.edu>
2014-05-07 17:51:07 -0700

    Added web listener node.

---
+++ scripts/weblistener.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from chatbot.msg import ChatMessage
+from flask import Flask
+from flask import render_template
+import rospy
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+  return render_template('index.html')
+
+class ChatbotWebSpeechRecognizer:
+  def __init__(self):
+    rospy.init_node('chatbot_web_listener')
+
+  def start(self):
+    app.run(host='0.0.0.0')
+
+def main():
+  recognizer = ChatbotWebSpeechRecognizer()
+  recognizer.start() 
+
+if __name__ == '__main__':
+  main()
