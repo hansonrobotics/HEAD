300e418c6fae147d172e0dfdd4bb2ea2660e0b9d
Gabrielius <gabrielius.m@gmail.com>
2014-06-27 17:43:39 +0800

    Added neutral emotion. Limit commad sending rate.

--- robo-interface.js
+++ robo-interface.js
@@ -1,22 +1,51 @@
+//Utility function. Builds a wrapper function around the given function and
+//makes sure it is not invoked in shorter time intervals than 'millis'. The
+//last call is guaranteed to make it after the timer goes out.
+var limitCallRate = function(millis, func) {
+  var timeout = null;
+  var last_args = null;
+
+  function fire() {
+    timeout = null;
+    if (last_args != null) {
+      args = last_args;
+      last_args = null;
+      timeout = setTimeout(fire, millis);
+      func.apply(null, args); //Apply the last saved arguments
+    }
+  }
+
+  return function() {
+    last_args = arguments;
+    if (timeout == null) {
+      fire();
+    }
+  };
+}
+
+//Main Class
 var RoboInterface = {
 
   $: $({}), //Event proxy. Events are triggered and bound to this object.
 
-  sendMotorCmd: function(confEntry, angle, speed, acc) {
+  //Keeps the incomming motor messages from flooding.
+  sendMotorCmd: limitCallRate(30, function() {
+    RoboInterface._sendMotorCmd.apply(RoboInterface, arguments);
+  }),
+
+  _sendMotorCmd: function(confEntry, angle, speed, acc) {
     var cmd = new ROSLIB.Message({
       id: confEntry.motorid,
       angle: Math.min(Math.max(angle, confEntry.min), confEntry.max),
       speed: speed || confEntry.speed,
       acceleration: acc || confEntry.acceleration
     });
-    this.motorCmdTopic.publish(cmd);
+    RoboInterface.motorCmdTopic.publish(cmd);
   },
 
   sendDefaultMotorCmds: function() {
     for (var i = 0; i < this.motorConf.length; i++) {
-      var confEntry = motorConf[i];
-      if (confEntry.motorid != undefined)
-        this.sendMotorCmd(confEntry, confEntry.default);
+      this._sendMotorCmd(this.motorConf[i], this.motorConf[i].default);
     };
     this.pointHead();
   },
@@ -37,17 +66,22 @@ var RoboInterface = {
     );
   },
 
-  pointHead: function(new_angles){
+  //Keeps the incomming motor messages from flooding.
+  pointHead: limitCallRate(30, function() {
+    RoboInterface._pointHead.apply(RoboInterface, arguments);
+  }),
+
+  _pointHead: function(new_angles){
     var angles = {yaw: 0, pitch: 0, roll: 0};
 
-    this.pointHead = function(new_angles) {
+    this._pointHead = function(new_angles) {
       $.extend(angles, new_angles);
       RoboInterface.pointHeadTopic.publish(
         new ROSLIB.Message(angles)
       );
     };
 
-    return this.pointHead(new_angles);
+    return this._pointHead(new_angles);
   },
 
   //Loads the config file and connects to ROS
