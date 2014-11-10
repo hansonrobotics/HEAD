a2040d5dcab6dd8f349db413509d157ee6a3598a
Vytas <vytas@hansonrobotics.com>
2014-11-10 16:28:46 +0800

    websocket address based on the protocol

--- SimpleHTTPSServer.py
+++ SimpleHTTPSServer.py
@@ -2,6 +2,6 @@ import BaseHTTPServer, SimpleHTTPServer
 import ssl
 
 httpd = BaseHTTPServer.HTTPServer(('0.0.0.0', 4443), SimpleHTTPServer.SimpleHTTPRequestHandler)
-httpd.socket = ssl.wrap_socket (httpd.socket, certfile='./server.pem', server_side=True)
+httpd.socket = ssl.wrap_socket (httpd.socket, keyfile='/ssl/ssl.key', certfile='/ssl/artrobot.cert', server_side=True)
 httpd.serve_forever()
 
--- rosbridge_launch.sh
+++ rosbridge_launch.sh
@@ -1,1 +1,1 @@
-roslaunch rosbridge_server rosbridge_websocket.launch ssl:=true certfile:=/catkin_ws/src/ros_motors_webui/server.pem keyfile:=/catkin_ws/src/ros_motors_webui/server.pem
+roslaunch rosbridge_server rosbridge_websocket.launch ssl:=true certfile:=/ssl/artrobot.cert keyfile:=/ssl/ssl.key port:=9092
