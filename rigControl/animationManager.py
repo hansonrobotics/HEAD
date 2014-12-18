018a81c5616f4ac3b73d02d2a36f172d70946874
Linas Vepstas <linasvepstas@gmail.com>
2014-12-18 15:17:49 -0600

    Assorted whitespace cleanup

--- rigControl/animationManager.py
+++ rigControl/animationManager.py
@@ -4,7 +4,7 @@ from . import actuators
 from .blendedNum import BlendedNum
 from .helpers import *
 
-import bpy	
+import bpy
 import random
 import time
 import imp
@@ -14,14 +14,14 @@ debug = True
 
 
 class AnimationManager():
-	
+
 	def __init__(self):
 		print('Starting AnimationManager singleton')
-		
+
 		# gesture params
 		self.gesturesList = []
 		self.emotionsList = []
-		
+
 		# tracking param
 		self.primaryHeadTargetLoc = BlendedNum([0,0,0], steps=10, smoothing=10)
 		self.secondaryHeadTargetLoc = BlendedNum([0,0,0], steps=10, smoothing=10)
@@ -54,12 +54,12 @@ class AnimationManager():
 
 
 	def keepAlive(self):
-		''' called every frame, used to dispatch animation actuators'''
+		'''Called every frame, used to dispatch animation actuators'''
 		self.idle += 1.0
 
 		if True:
 			actuators.idleCycle(self)
-			
+
 		if True:
 			actuators.breathingCycle(self, self.breathRate, self.breathIntensity)
 
@@ -74,10 +74,9 @@ class AnimationManager():
 
 		if True and self.randomFrequency('emotionJitter', 20):
 			actuators.emotionJitter(self)
-		
 
 
-	# show all attributes
+	# Show all attributes
 	def __repr__(self):
 		string = ""
 		for attr, value in sorted(self.__dict__.items()):
@@ -94,17 +93,17 @@ class AnimationManager():
 			raise Exception('No gesture matching name is found')
 			return
 
-		# check value for sanity
+		# Check value for sanity
 		checkValue(repeat, 1, 1000)
 		checkValue(speed, 0.1, 10)
 		checkValue(magnitude, 0, 1)
 		checkValue(priority, 0, 1)
 
-		# create NLA track
+		# Create NLA track
 		newTrack = self.deformObj.animation_data.nla_tracks.new()
 		newTrack.name = name
 
-		# create strip
+		# Create strip
 		newStrip = newTrack.strips.new(name=name, start=1, action=actionDatablock)
 		duration = (newStrip.frame_end - newStrip.frame_start)
 		newStrip.blend_type = 'ADD'
@@ -118,8 +117,9 @@ class AnimationManager():
 			newStrip.use_animated_influence = True
 			newStrip.influence = magnitude
 
-		# create object and add to list
-		g = Gesture(name, newTrack, newStrip, duration=duration, speed=speed, magnitude=magnitude, priority=priority, repeat=repeat)
+		# Create object and add to list
+		g = Gesture(name, newTrack, newStrip, duration=duration, speed=speed, \
+			 magnitude=magnitude, priority=priority, repeat=repeat)
 		self.gesturesList.append(g)
 
 
@@ -133,7 +133,7 @@ class AnimationManager():
 
 
 	def setEmotion(self, emotionDict):
-		''' set the emotion param of the character'''
+		'''Set the emotion param of the character'''
 		for emotionName, data in emotionDict.items():
 			try:
 				control = self.bones['EMO-'+emotionName]
@@ -148,14 +148,14 @@ class AnimationManager():
 						emotion.magnitude.target = data['magnitude']
 						emotion.duration = data['duration']
 						found = True
-				
+
 				if not found:
 					emotion = Emotion(emotionName, magnitude = BlendedNum(data['magnitude'], steps = 10, smoothing = 10), duration = data['duration'])
 					self.emotionsList.append(emotion)
 
 
 	def setPrimaryTarget(self, loc):
-		''' set the target used by eye and face tracking '''
+		'''Set the target used by eye and face tracking '''
 		# compute distance from previous eye position
 		distance = computeDistance(loc, self.primaryEyeTargetLoc.current)
 
@@ -165,10 +165,10 @@ class AnimationManager():
 
 		self.primaryHeadTargetLoc.target = loc
 		self.primaryEyeTargetLoc.target = loc
-			
+
 
 	def terminate(self):
-		''' house keeping at the end of the run'''
+		'''House-keeping at the end of the run'''
 		# remove all leftover gestures
 		for gesture in self.gesturesList:
 			self.deformObj.animation_data.nla_tracks.remove(gesture.trackRef)
@@ -185,7 +185,7 @@ class AnimationManager():
 
 
 	def randomFrequency(self, name, hz):
-		''' returns a random true/false based on a hertz value as input'''
+		'''Returns a random true/false based on a hertz value as input'''
 		try:
 			oldTime = self.lastTriggered[name]
 		except KeyError:
@@ -201,19 +201,17 @@ class AnimationManager():
 			return False
 
 
-
-			
 class Emotion():
-	''' represents an emotion'''
+	'''Represents an emotion'''
 	def __init__(self, name, magnitude, duration):
 		self.name = name
 		self.magnitude = magnitude
 		self.duration = duration
 		self.priority = 0
 
-			
+
 class Gesture():
-	''' represents a blender actions'''
+	'''Represents a blender actions'''
 	def __init__(self, name, track, strip, duration, speed, magnitude, priority, repeat):
 		self.name = name
 		self.duration = duration
@@ -224,7 +222,6 @@ class Gesture():
 
 		self.trackRef = track
 		self.stripRef = strip
-
 
 
 def init():
