4becd61110f969ce5bdde81f7aa6b36b11a3bd68
Linas Vepstas <linasvepstas@gmail.com>
2014-12-30 11:38:06 -0600

    Add gaze and face targets to UI

--- rigControl/blenderUI.py
+++ rigControl/blenderUI.py
@@ -94,13 +94,22 @@ class BLRigControl(bpy.types.Panel):
 
 		# Warning: X and Y Axis are rotated to accomodate Command Lisenter
 		col = layout.column(align = True)
-		col.operator('eva.debug', text='Up').action = 'commands.setFaceTarget([1, 0, 1])'
+		col.operator('eva.debug', text='Face Up').action = 'commands.setFaceTarget([1, 0, 1])'
 		row = col.row(align=True)
 		row.operator('eva.debug', text='Left').action = 'commands.setFaceTarget([1, -1, 0])'
 		row.operator('eva.debug', text='Centre').action = 'commands.setFaceTarget([1, 0,0])'
 		row.operator('eva.debug', text='Right').action = 'commands.setFaceTarget([1, 1, 0])'
-		col.operator('eva.debug', text='Down').action = 'commands.setFaceTarget([1, 0, -1])'
-		col.operator('eva.debug', text='Nil').action = 'commands.setFaceTarget([0, 0, 0])'
+		col.operator('eva.debug', text='Face Down').action = 'commands.setFaceTarget([1, 0, -1])'
+		col.operator('eva.debug', text='Face Nil').action = 'commands.setFaceTarget([0, 0, 0])'
+
+		col = layout.column(align = True)
+		col.operator('eva.debug', text='Gaze Up').action = 'commands.setGazeTarget([1, 0, 1])'
+		row = col.row(align=True)
+		row.operator('eva.debug', text='Left').action = 'commands.setGazeTarget([1, -1, 0])'
+		row.operator('eva.debug', text='Centre').action = 'commands.setGazeTarget([1, 0,0])'
+		row.operator('eva.debug', text='Right').action = 'commands.setGazeTarget([1, 1, 0])'
+		col.operator('eva.debug', text='Gaze Down').action = 'commands.setGazeTarget([1, 0, -1])'
+		col.operator('eva.debug', text='Gaze Nil').action = 'commands.setGazeTarget([0, 0, 0])'
 
 		row = layout.row()
 		eva = bpy.evaAnimationManager
