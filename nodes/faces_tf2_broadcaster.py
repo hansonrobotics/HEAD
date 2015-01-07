7ea0891201f139be229677e9223331d68d439d6f
Vytas <vytas@hansonrobotics.com>
2015-01-07 18:26:26 +0800

    Face tracking with eye_camera correction

--- nodes/faces_tf2_broadcaster.py
+++ nodes/faces_tf2_broadcaster.py
@@ -1,33 +1,103 @@
 #!/usr/bin/env python
 import rospy
-import tf2_ros
+import tf
 from pi_face_tracker.msg import *
 from geometry_msgs.msg import Quaternion
 from geometry_msgs.msg import TransformStamped
+from geometry_msgs.msg import PointStamped
 from geometry_msgs.msg import Transform
 from geometry_msgs.msg import Vector3
 from std_msgs.msg import Header
 import time
+import math
 
 broadcasters = {}
 
-def handle_faces(msg):
-    f= Faces()
+class FacesPublisher:
 
-    for i, face in enumerate(msg.faces):
-        if not i in broadcasters.keys():
-            broadcasters[i] =  tf2_ros.TransformBroadcaster()
-        transform = TransformStamped(header=Header(stamp=rospy.Time.now(), frame_id="camera"),
-                             transform=Transform(translation=Vector3(face.point.x,face.point.y,face.point.z),
-                                                 rotation=Quaternion(0,0,0,1)),
-                             child_frame_id="Face" + str(face.id)
-                            )
-        broadcasters[i].sendTransform(transform)
-        time.sleep(0.01) # Need to investigate why we cant publish multiple messages at same time
+    def __init__(self):
+        # distance allowed by two point to merge them
+        self.max_distance = rospy.get_param('max_distance', 0.5)
+        # angle for faces to be marked as same, to be refined in future
+        #self.max_angle = rospy.get_param('max_angle', 0.085)
+        rospy.Subscriber('camera/face_locations',
+                     pi_face_tracker.msg.Faces,
+                     self.handle_faces)
+        rospy.Subscriber('eye_camera/face_locations',
+                     pi_face_tracker.msg.Faces,
+                     self.handle_eye_faces)
+        #Last faces got from body camera
+        self.last_faces = {}
+        # stores delta information for the faces
+        self.delta = {}
+        self.broadcaster =  tf.TransformBroadcaster()
+        self.listener = tf.TransformListener()
+        # waiting for camera and eye transforms
+        self.listener.waitForTransform('camera','world', rospy.Time(0), rospy.Duration(2.0))
+        self.last_faces_time = rospy.Time.now()
+        #self.buffer.lookup_transform()
+
+    def handle_faces(self, msg):
+        self.last_faces = msg.faces
+        self.last_faces_time = now = rospy.Time.now()
+        for i, face in enumerate(msg.faces):
+            # tr= TransformStamped(header=Header(stamp=rospy.Time.now(), frame_id="camera"),
+            #                  transform=Transform(translation=Vector3(face.point.x,face.point.y,face.point.z),
+            #                                      rotation=Quaternion(0,0,0,1)),
+            #                  child_frame_id="face_base" + str(face.id)
+            #                 )
+            # self.broadcaster.sendTransform(tr)    --- tf2 version
+
+            self.broadcaster.sendTransform((face.point.x,face.point.y,face.point.z),(0,0,0,1),
+                                            self.last_faces_time,"face_base" + str(face.id),"camera")
+            time.sleep(0.005) # Need to investigate why we cant publish multiple messages at same time
+            # Publish eye detected face
+            # t = TransformStamped(header=Header(stamp=rospy.Time.now(), frame_id="face_base" + str(face.id)),
+            #                      child_frame_id="Face" + str(face.id)
+            #                      ) -- tf2 version
+            p = (0,0,0)
+            if face.id in self.delta:
+                #t.transform.translation = self.delta[face.id]
+                d = self.delta[face.id]
+                p=(d.x,d.y,d.z)
+
+            #self.broadcaster.sendTransform(t)  --- tf2 version
+            self.broadcaster.sendTransform(p, (0, 0, 0, 1),  self.last_faces_time,
+                                           "Face" + str(face.id), "face_base" + str(face.id))
+
+
+    def handle_eye_faces(self, msg):
+        # Clear corrections everytime frame from eyes processed
+        self.delta = {}
+        for i, eye_face in enumerate(msg.faces):
+            # used for debug
+            #self.broadcaster.sendTransform((eye_face.point.x,eye_face.point.y,eye_face.point.z),(0,0,0,1),
+            #                                self.last_faces_time,"eye_face" + str(eye_face.id),"right_eye")
+            ps = PointStamped()
+            ps.point = eye_face.point
+            ps.header=Header(stamp= rospy.Time(0), frame_id="right_eye")
+            for j, face in enumerate(self.last_faces):
+                try:
+
+                    pst = self.listener.transformPoint("face_base"+str(face.id),ps)
+                    if self._sameFace(pst):
+                        self.delta[face.id] = pst.point
+                except:
+                    rospy.logwarn("No transform")
+                    continue
+
+    # Checks if we can merge two points from different cameras to single face.
+    # ps : PointStamped relative to the face we are checking
+
+    def _sameFace(self, ps):
+        # To be refined based on real data
+        if math.sqrt(ps.point.x**2 + ps.point.y**2 + ps.point.z**2) <= self.max_distance:
+            return True
+        return False
+
+
 
 if __name__ == '__main__':
     rospy.init_node('faces_tf2_broadcaster')
-    rospy.Subscriber('/faces3d',
-                     pi_face_tracker.msg.Faces,
-                     handle_faces)
+    FP = FacesPublisher()
     rospy.spin()
