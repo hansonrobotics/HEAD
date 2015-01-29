84d6f55a3ef91b05dc7263ea9a9368662a56a584
Gabrielius Mickevicius <gabrielius.m@gmail.com>
2015-01-29 16:58:48 +0200

    Load roscom when not in autostart mode

--- loader.py
+++ loader.py
@@ -10,5 +10,18 @@ def register():
 	# import package and force refresh for dev
 	import rigControl
 	imp.reload(rigControl)
-	
+
+	# Start ROS node too..
+	# If ROS is found, we try to load the ROS node, else not.
+	from rigControl import commands, CommandListener
+	import importlib
+	try:
+		importlib.import_module('rospy')
+		import rosrig
+
+		node = rosrig.init(commands.EvaAPI())
+		CommandListener.register_cmd_source(node)
+	except ImportError:
+		print('No ROS found')
+
 register()
