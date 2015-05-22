fef07a4f3bf7c0b78cf4ebc387a2ab6cead70c47
Vytas <vytas@hansonrobotics.com>
2015-05-22 10:05:05 +0800

    config format fix. The topics based opnn hardware

--- src/HardwareFactory.py
+++ src/HardwareFactory.py
@@ -23,11 +23,14 @@ class HardwareBase:
 
     if not self.__class__._topics.has_key(args["topic"]):
       self.__class__._topics[args["topic"]] = rospy.Publisher(
-        args["topic"]+"/command", self.messageType, queue_size=30
+        self.getTopicName(args["topic"]), self.messageType, queue_size=30
       )
     self.pub = self.__class__._topics[args["topic"]]
     self.args = args
 
+  def getTopicName(self, topic):
+      ''' Default topic format '''
+      return topic + "/command"
 
 class Pololu(HardwareBase):
   messageType = MotorCommand
@@ -52,6 +55,10 @@ class Dynamixel(HardwareBase):
 
   def turn(self, angle):
     self.pub.publish(angle)
+
+  def getTopicName(self, topic):
+      ''' Default topic format '''
+      return topic + "_controller/command"
 
 
 _hardware_classes = {
--- src/pau2motors_node.py
+++ src/pau2motors_node.py
@@ -19,10 +19,8 @@
 # 02110-1301  USA
 #
 import rospy
-import Utils
 from pau2motors.msg import pau
 from Pau2Motors import Pau2Motors
-import os
 from optparse import OptionParser
 
 def get_namespaces_containing(name_piece):
@@ -72,36 +70,22 @@ class Pau2MotorsNode:
 parser = OptionParser()
 if __name__ == '__main__':
 
-  def print_help():
-    msg = "Change namespace with `export ROS_NAMESPACE=<ns>`"
-    ns = get_namespaces_containing(param_name)
-    if len(ns) > 0:
-      msg += "\nCurrent valid <ns>: %s." % ", ".join(ns)
-    else:
-      msg += "\nNo current valid <ns> found."
-    msg += "\nCheck robots_config repo to get more config options."
-    print(msg)
-
   # node specific settings
   config = rospy.get_param("pau2motors", None)
-  # motors
+  # motors from universal motors parameter.
   motors = rospy.get_param("motors", None)
-
   if config and motors:
-    config["motors"] = motors
+    config["motors"] = dict([i["name"],i] for i in motors)
     Pau2MotorsNode(config)
     rospy.loginfo(
       "Loaded '%s' from param server.",
-      rospy.get_namespace() + param_name
+      rospy.get_namespace() + "pau2motors"
     )
-    print_help()
     rospy.loginfo("Started")
     rospy.spin()
   else:
     print(
       "Couldn't find '%s' param in namespace '%s'." %
-     (param_name, rospy.get_namespace())
+     ("pau2motors", rospy.get_namespace())
     )
-    print_help()
-    print
 
