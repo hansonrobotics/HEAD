19408d08b3e92f3d06ab78ee040a82be8bfb1cf6
Linas Vepstas <linasvepstas@gmail.com>
2015-06-23 09:28:55 +0800

    Add new message to the CMakefile

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "roscom-blender-api",
-    version = "0.1",
+    version = "0.2",
     package_dir = {'': 'src',},
     packages = find_packages('src'),
     entry_points = {
