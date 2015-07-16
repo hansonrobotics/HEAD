503ba42c768b46662019b99efa199bd641713e84
Wenwei Huang <dut.hww@gmail.com>
2015-07-16 17:00:31 +0800

    Fix message delay

--- test/test_eva_behavior.py
+++ test/test_eva_behavior.py
@@ -15,6 +15,15 @@ from testing_tools import wait_for_message
 
 PKG = 'eva_behavior'
 
+from rospy import SubscribeListener
+
+class SwitchSubscribeListener(SubscribeListener):
+    def __init__(self):
+        self.has_sub = False
+
+    def peer_subscribe(self, topic_name, topic_publish, peer_publish):
+        self.has_sub = True
+
 class EvaBehaviorTest(unittest.TestCase):
     def setUp(self):
         self.run_id = 'test_eva_behavior'
@@ -32,17 +41,22 @@ class EvaBehaviorTest(unittest.TestCase):
         self.runner.stop()
 
     def test_btree_on_off(self):
-        while not self.runner.is_node_running(self.node):
-            time.sleep(0.2)
-
         if self.runner.is_node_running(self.node):
+            l = SwitchSubscribeListener()
             pub = rospy.Publisher(
-                '/behavior_switch', String, queue_size=30, latch=True)
+                '/behavior_switch', String, queue_size=30,
+                subscriber_listener=l)
+            while not l.has_sub:
+                time.sleep(0.2)
+            timeout = 60
             pub.publish(String('btree_on'))
-            msg = wait_for_message('/blender_api/set_gesture', SetGesture, 30)
+            msg = wait_for_message(
+                '/blender_api/set_gesture', SetGesture, timeout)
+            rospy.loginfo("Got msg %s" % msg)
             self.assertIsNotNone(msg)
             pub.publish(String('btree_off'))
-            msg = wait_for_message('/blender_api/set_gesture', SetGesture, 30)
+            msg = wait_for_message(
+                '/blender_api/set_gesture', SetGesture, timeout)
             self.assertIsNone(msg)
 
 if __name__ == '__main__':
