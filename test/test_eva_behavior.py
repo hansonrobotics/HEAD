97881f8cb2796c764c377f7e2a2369cb79d082f6
Wenwei Huang <dut.hww@gmail.com>
2015-07-17 16:52:47 +0800

    Add face interactiontest and refactoring

--- test/test_eva_behavior.py
+++ test/test_eva_behavior.py
@@ -8,21 +8,14 @@ import time
 import rospy
 import roslaunch
 from roslaunch import core
+import rostopic
 
-from std_msgs.msg import String
-from blender_api_msgs.msg import SetGesture
+from blender_api_msgs.msg import SetGesture, EmotionState
 from testing_tools import wait_for_message
 
 PKG = 'eva_behavior'
 
 from rospy import SubscribeListener
-
-class SwitchSubscribeListener(SubscribeListener):
-    def __init__(self):
-        self.has_sub = False
-
-    def peer_subscribe(self, topic_name, topic_publish, peer_publish):
-        self.has_sub = True
 
 class EvaBehaviorTest(unittest.TestCase):
     def setUp(self):
@@ -30,34 +23,69 @@ class EvaBehaviorTest(unittest.TestCase):
         config = roslaunch.config.ROSLaunchConfig()
         self.node = core.Node(
             package='eva_behavior', node_type='main.py',
-            name='eva_behavior')
+            name='Eva_behavior')
         config.add_node(self.node)
         self.runner = roslaunch.launch.ROSLaunchRunner(
             self.run_id, config)
         self.runner.launch()
-        rospy.init_node('test_eva_behavior')
+        #rospy.init_node('test_eva_behavior')
 
     def tearDown(self):
         self.runner.stop()
 
+    def behavior_switch(self, msg):
+        while not self.runner.is_node_running(self.node):
+            time.sleep(0.01)
+        topic = '/behavior_switch'
+        pub, msg_class = rostopic.create_publisher(
+            topic, 'std_msgs/String', True)
+        pub.publish(msg_class(msg))
+
     def test_btree_on_off(self):
-        if self.runner.is_node_running(self.node):
-            l = SwitchSubscribeListener()
-            pub = rospy.Publisher(
-                '/behavior_switch', String, queue_size=30,
-                subscriber_listener=l)
-            while not l.has_sub:
-                time.sleep(0.2)
-            timeout = 60
-            pub.publish(String('btree_on'))
-            msg = wait_for_message(
-                '/blender_api/set_gesture', SetGesture, timeout)
-            rospy.loginfo("Got msg %s" % msg)
-            self.assertIsNotNone(msg)
-            pub.publish(String('btree_off'))
-            msg = wait_for_message(
-                '/blender_api/set_gesture', SetGesture, timeout)
-            self.assertIsNone(msg)
+        timeout = 60
+        self.behavior_switch('btree_on')
+        msg = wait_for_message(
+            '/blender_api/set_gesture', SetGesture, timeout)
+        rospy.loginfo("Got msg %s" % msg)
+        self.assertIsNotNone(msg)
+
+        self.behavior_switch('btree_off')
+        msg = wait_for_message(
+            '/blender_api/set_gesture', SetGesture, timeout)
+        self.assertIsNone(msg)
+
+    def test_face_interaction1(self):
+        timeout = 30
+        self.behavior_switch('btree_on')
+
+        pub, msg_class = rostopic.create_publisher(
+            '/blender_api/available_emotion_states',
+            'blender_api_msgs/AvailableEmotionStates', True)
+        pub.publish(msg_class(['happy']))
+
+        pub, msg_class = rostopic.create_publisher(
+            '/camera/face_event', 'pi_face_tracker/FaceEvent', True)
+        pub.publish(msg_class('new_face', 1))
+
+        ges_msg = wait_for_message(
+            '/blender_api/set_gesture', SetGesture, timeout)
+        self.assertIn(ges_msg.name, ['nod-1', 'nod-2'])
+        emo_msg = wait_for_message(
+            '/blender_api/set_emotion_state', EmotionState, timeout)
+        self.assertIn(emo_msg.name, ['happy'])
+
+    def test_face_interaction2(self):
+        timeout = 30
+        self.behavior_switch('btree_on')
+
+        pub, msg_class = rostopic.create_publisher(
+            '/camera/face_event', 'pi_face_tracker/FaceEvent', True)
+        pub.publish(msg_class('new_face', 1))
+
+        positive_emotions = ['happy', 'comprehending', 'engaged']
+        emo_msg = wait_for_message(
+            '/blender_api/set_emotion_state', EmotionState, timeout)
+        self.assertIn(emo_msg.name, positive_emotions)
 
 if __name__ == '__main__':
     import rostest
