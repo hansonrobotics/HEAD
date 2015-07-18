3eb9e1dc11d81cbb0e81669e7a873f7c3110b54d
Gabrielius Mickevicius <gabrielius.m@gmail.com>
2015-07-18 21:41:03 +0800

    add wrappers and docstrings

--- src/blender_api/rigControl/blendedNum.py
+++ src/blender_api/rigControl/blendedNum.py
@@ -1,106 +1,177 @@
-# Implements a numeric object that allow blending and smoothing of values
-# access using .current, set using .target, must be manually interpolated using .blend() every frame
-
-import collections
+from collections import deque
+from math import tan, atan, sqrt
 
 class BlendedNum():
-    ''' represents a number or vec3 float that is smoothly blended'''
-    def __init__(self, value, steps = 20, smoothing = 20):
-        if type(value) is float or type(value) is int:
-            self._target = value
-            self._old = value
-            self._current = value
-            self._movingAverage = collections.deque(5*[0], smoothing)
-            self.isVector = False
-        else:
-            self._target = value.copy()
-            self._old = value.copy()
-            self._current = value.copy()
-            self._movingAverage = collections.deque(5*[value.copy()], smoothing)
-            self.isVector = True
+    def __init__(self, value, transition=None):
+        self._current = value
+        self._target = Target()
 
-        self._steps = steps
-
-
-    def __repr__(self):
-        allStr = 'BlendedNum:' + str((self._current, self._old, self._target))
-        return allStr
+        self.transition = transition or Transitions.chain(
+            Transitions.linear(1.0),
+            Transitions.moving_average(1.0)
+        )
+        self.transition.send(None)
 
     @property
     def current(self):
-        ''' return moving average instead of raw lerp value '''
-        if self.isVector:
-            return [float(sum(col))/len(col) for col in zip(*list(self._movingAverage))]
-        else:
-            return sum(self._movingAverage)/len(self._movingAverage)
-
-    @current.setter
-    def current(self, value):
-        self._current = value
-
+        return self._current
 
     @property
     def target(self):
+        """ The target only has a getter to discourage overriding it.
+        Using target.add(val) as the default method will result in
+        more scalable use of BlendedNum instances. """
         return self._target
 
-    @target.setter
-    def target(self, value):
-        if self.isVector:
-            self._target = value.copy()
-            self._old = self._current.copy()
-        else:
-            self._target = value
-            self._old = self._current
+    def blend(self, time, dt):
+        """ Updates the 'current' value and clears the 'target' to encourage
+        adding all target components before each 'blend' call."""
+        target_val = self._target.clear()
+        self._current = self.transition.send((target_val, time, dt))
 
 
-    @property
-    def steps(self):
-        return self._steps
-    @steps.setter
-    def steps(self, value):
-        self._steps = value
+class Transitions:
 
-    # @property
-    # def smoothing(self):
-    #     return self._movingAverage
-    # @smoothing.setter
-    # def smoothing(self, smoothing):
-    #     self._movingAverage = collections.deque(5*[0], smoothing)
+    @staticmethod
+    def linear(speed):
+        # 'target' type: List[int]
+        target, time, dt = yield None
+        current = target
+        while True:
+            # Find the distance to 'target'
+            displacement = [a - b for a, b in zip(target, current)]
+            distance = sqrt(sum(a**2 for a in displacement))
 
-    def blend(self):
-        #store old value to moving average:
-        if self.isVector:
-            self._movingAverage.append(self._current.copy())
-        else:
-            self._movingAverage.append(self._current)
+            if distance > speed * dt:
+                # Find the unit direction vector to 'target'
+                direction = [a / distance for a in displacement]
 
-        if self.isVector and len(self._target)==3:
-            # trilinear interpolation
-            delta = [0,0,0]
-            delta[0] = (self._target[0] - self._old[0]) / self._steps
-            delta[1] = (self._target[1] - self._old[1]) / self._steps
-            delta[2] = (self._target[2] - self._old[2]) / self._steps
+                # Move 'current' to that direction in proportion to speed and dt.
+                current = [a + b * speed * dt for a, b in zip(current, direction)]
+            else:
+                current = target
 
-            self._current[0] += delta[0]
-            self._current[1] += delta[1]
-            self._current[2] += delta[2]
+            target, time, dt = yield current
 
-            if (delta[0] > 0 and self._current[0] > self._target[0]) or (delta[0] < 0 and self._current[0] < self._target[0]):
-                self._current[0] = self._target[0]
-                self._old[0] = self._target[0]
-            if (delta[1] > 0 and self._current[1] > self._target[1]) or (delta[1] < 0 and self._current[1] < self._target[1]):
-                self._current[1] = self._target[1]
-                self._old[1] = self._target[1]
-            if (delta[2] > 0 and self._current[2] > self._target[2]) or (delta[2] < 0 and self._current[2] < self._target[2]):
-                self._current[2] = self._target[2]
-                self._old[2] = self._target[2]
 
-        else:
-            # linear interpolation
-            deltaValue = (self._target - self._old) / self._steps
-            self._current += deltaValue
+    @staticmethod
+    def moving_average(duration):
+        """ Duration in seconds. """
+        # 'target' type: List[int]
+        target, time, dt = yield None
+        buffer = WeightBuffer()
+        while True:
+            buffer.append((target, dt))
+            buffer.cut_to_fit(duration)
+            target, time, dt = yield buffer.weighted_mean()
 
-            if (deltaValue > 0 and self._current > self._target) or (deltaValue < 0 and self._current < self._target):
-                self._current = self._target
-                self._old = self._target
-                return
+    @staticmethod
+    def chain(*transitions):
+        for trans in transitions:
+            trans.send(None)
+        target, time, dt = yield None
+        while True:
+            value = target
+            for trans in transitions:
+                value = trans.send((value, time, dt))
+            target, time, dt = yield value
+
+
+class Wrappers:
+
+    @staticmethod
+    def in_spherical(origin, radius=3):
+        x0, y0, z0 = origin
+
+        def cartesian_to_spherical():
+            target, _, _ = yield None
+            while True:
+                x, y, z = target
+                x, y, z = x-x0, y-y0, z-z0
+                yaw = atan(x / y)
+                pitch = atan(z / y)
+                target, _, _ = yield [yaw, pitch]
+
+        def spherical_to_cartesian():
+            target, _, _ = yield None
+            while True:
+                yaw, pitch = target
+                y = radius / sqrt(1 + tan(yaw)**2 + tan(pitch)**2)
+                x = y * tan(yaw)
+                z = y * tan(pitch)
+                target, _, _ = yield [x+x0, y+y0, z+z0]
+
+        return cartesian_to_spherical(), spherical_to_cartesian()
+
+    @staticmethod
+    def wrap(transition, wrapper):
+        prepend_transition, append_transition = wrapper
+        return Transitions.chain(prepend_transition, transition, append_transition)
+
+
+class WeightBuffer(deque):
+    """ Required element type: (List[float], float).
+    The list being a value vector of arbitrary length and the last float being
+    the weight of the element.
+
+    These weights enable useful methods like 'weighted_mean' and 'cut_to_fit'.
+    See their docstrings. """
+
+    def weighted_mean(self):
+        """ Return a weighted average vector. """
+        result = []
+
+        vectors, weights = zip(*self)
+        for dimension in zip(*vectors):
+            single_dimension_result = WeightBuffer(zip(dimension, weights))._weighted_mean_scalar()
+            result.append(single_dimension_result)
+
+        return result
+
+    def _weighted_mean_scalar(self):
+        weighted_sum = sum(val * weight for val, weight in self)
+        sum_of_weights = sum(weight for val, weight in self)
+        return weighted_sum / sum_of_weights
+
+    def cut_to_fit(self, capacity):
+        """ Works similary to a circular buffer, but in terms of a fixed
+        floating number weight capacity instead of a fixed number of elements.
+
+        Trim the left side until the buffer matches the given 'capacity'.
+        E.g. WeightBuffer([(..., 0.2), (..., 0.8), (..., 0.4)]).cut_to_fit(1.0)
+        would result in WeightBuffer([(..., 0.6), (..., 0.4)])
+        """
+        overflow = sum(weight for val, weight in self) - capacity
+
+        if overflow <= 0:
+            return
+
+        while overflow > 0:
+            val, weight = self.popleft()
+            overflow -= weight
+
+        if overflow < 0:
+            self.appendleft((val, abs(overflow)))
+
+def fixed_fps(transition, fps):
+    transition.send(None)
+    target = yield None
+    dt = 1.0/fps
+    time = 0
+    while True:
+        target = yield transition.send((target, time, dt))
+        time += dt
+
+
+class Target:
+
+    def __init__(self):
+        self._accumulator = 0
+
+    def add(self, val):
+        self._accumulator += val
+
+    def clear(self):
+        value = self._accumulator
+        self._accumulator = 0
+        return value
