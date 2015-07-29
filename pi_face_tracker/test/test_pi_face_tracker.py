61f71162c25dd3577bb52632b70cacada92f1a83
Wenwei Huang <dut.hww@gmail.com>
2015-07-29 18:04:20 +0800

    Refactor

--- pi_face_tracker/test/test_pi_face_tracker.py
+++ pi_face_tracker/test/test_pi_face_tracker.py
@@ -8,8 +8,13 @@ import time
 
 import rospkg
 import roslaunch
-from testing_tools import rosbag_msg_generator, play_rosbag
+from testing_tools import (rosbag_msg_generator, play_rosbag,
+                        get_rosbag_file, wait_for_message, wait_for_messages)
 from pi_face_tracker.msg import FaceEvent, Faces
+
+from roslaunch import nodeprocess
+nodeprocess._TIMEOUT_SIGINT = 2
+nodeprocess._TIMEOUT_SIGTERM = 1
 
 CWD = os.path.abspath(os.path.dirname(__file__))
 PKG = 'pi_face_tracker'
@@ -26,109 +31,47 @@ class PiFaceTrackerTest(unittest.TestCase):
                 'test_pi_face_tracker', config)
             self.runner.launch()
 
-        self.test_data_path = '%s/test_data' % CWD
-
     def tearDown(self):
         self.runner.stop()
 
-    def test_face_out(self, ):
-        bag_file = '%s/face_out.bag' % self.test_data_path
-        job = play_rosbag([bag_file, '-q'])
-
-        face_detected = False
-        face_location_msg = None
-        face_event_msg = None
-        timeout = 2
-        while job.is_alive():
-            try:
-                face_location_msg = rospy.wait_for_message(
-                    '/camera/face_locations', Faces, timeout)
-            except rospy.exceptions.ROSException as e:
-                rospy.loginfo(e)
-                face_location_msg = None
-            if face_location_msg and face_location_msg.faces:
-                self.assertEqual(len(face_location_msg.faces), 1)
-                face = face_location_msg.faces[0]
-                self.assertGreater(face.attention, 0.99)
-
-            try:
-                face_event_msg = rospy.wait_for_message(
-                    '/camera/face_event', FaceEvent, timeout)
-            except rospy.exceptions.ROSException as e:
-                rospy.loginfo(e)
-                face_event_msg = None
-            if face_event_msg:
-                if not face_detected:
-                    self.assertEqual(face_event_msg.face_event, 'new_face')
-                    face_detected = True
-                else:
-                    self.assertEqual(face_event_msg.face_event, 'lost_face')
-                    face_detected = False
-        job.join()
-
-    def test_face_in(self):
-        bag_file = '%s/face_in.bag' % self.test_data_path
-        job = play_rosbag(bag_file)
-
-        face_detected = False
-        face_location_msg = None
-        face_event_msg = None
-        timeout = 2
-        while job.is_alive():
-            try:
-                face_location_msg = rospy.wait_for_message(
-                    '/camera/face_locations', Faces, timeout)
-            except rospy.exceptions.ROSException as e:
-                rospy.loginfo(e)
-                face_location_msg = None
-            if face_location_msg and face_location_msg.faces:
-                self.assertEqual(len(face_location_msg.faces), 1)
-                face = face_location_msg.faces[0]
-                self.assertGreater(face.attention, 0.99)
-
-            try:
-                face_event_msg = rospy.wait_for_message(
-                    '/camera/face_event', FaceEvent, timeout)
-            except rospy.exceptions.ROSException as e:
-                rospy.loginfo(e)
-                face_event_msg = None
-            if face_event_msg:
-                if not face_detected:
-                    self.assertEqual(face_event_msg.face_event, 'new_face')
-                    face_detected = True
-                else:
-                    self.assertEqual(face_event_msg.face_event, 'lost_face')
-                    face_detected = False
-        job.join()
-
     def test_noface(self):
-        bag_file = '%s/noface.bag' % self.test_data_path
+        bag_file = get_rosbag_file('empty')
         job = play_rosbag([bag_file, '-q'])
-
-        face_location_msg = None
-        face_event_msg = None
-        timeout = 2
+        face_event_msg, face_location_msg = None, None
+        fps = 25
+        wait = 1.0/fps
         while job.is_alive():
-            try:
-                face_location_msg = rospy.wait_for_message(
-                    '/camera/face_locations', Faces, timeout)
-            except rospy.exceptions.ROSException as e:
-                rospy.loginfo(e)
-                face_location_msg = None
+            face_location_msg, face_event_msg = wait_for_messages(
+                ['/camera/face_locations', '/camera/face_event'],
+                [Faces, FaceEvent], wait)
             self.assertIsNone(face_location_msg)
-
-            try:
-                face_event_msg = rospy.wait_for_message(
-                    '/camera/face_event', FaceEvent, timeout)
-            except rospy.exceptions.ROSException as e:
-                rospy.loginfo(e)
-                face_event_msg = None
             self.assertIsNone(face_event_msg)
 
         job.join()
 
+    def test_face_in_out(self):
+        face_event_msg, face_location_msg = None, None
+        fps = 25
+        wait = 1.0/fps
+        for _, _ in rosbag_msg_generator(get_rosbag_file('face')):
+            face_location_msg, face_event_msg = wait_for_messages(
+                ['/camera/face_locations', '/camera/face_event'],
+                [Faces, FaceEvent], wait)
+            if face_location_msg and face_location_msg.faces:
+                self.assertEqual(len(face_location_msg.faces), 1)
+                face = face_location_msg.faces[0]
+                self.assertGreater(face.attention, 0.99)
+            if face_event_msg:
+                self.assertEqual(face_event_msg.face_event, 'new_face')
+
+        for _, _ in rosbag_msg_generator(get_rosbag_file('empty')):
+            face_event_msg = wait_for_message(
+                '/camera/face_event', FaceEvent, wait)
+            if face_event_msg:
+                self.assertEqual(face_event_msg.face_event, 'lost_face')
+
+
 if __name__ == '__main__':
     import rostest
     rostest.rosrun(PKG, 'pi_face_tracker', PiFaceTrackerTest)
-    #unittest.main()
 
