c4e47aeaa6a71aab9a94e63a644de1f92b200470
Vytas <vytas@hansonrobotics.com>
2015-08-11 15:49:51 +0800

    Rearranged directory structure for motors safety node

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import os
+import os.path
 
 from flask import Flask, send_from_directory, request
 import reporter
@@ -8,12 +9,13 @@ import yaml
 import math
 import os.path
 from optparse import OptionParser
+from configs import *
 
 json_encode = json.JSONEncoder().encode
 
 app = Flask(__name__, static_folder='../public/')
 rep = reporter.Reporter(os.path.dirname(os.path.abspath(__file__)) + '/checks.yaml')
-
+config_root = os.path.dirname(os.path.abspath(__file__))
 
 @app.route('/')
 def send_index():
@@ -27,65 +29,29 @@ def send_status():
 
 @app.route('/motors/get/<robot_name>')
 def get_motors(robot_name):
-    config_root = "/catkin_ws/src/robots_config/" + robot_name + "/"
-    motors = read_yaml(config_root + "motors.yaml")
-    calibration = {}
-
-    for motor in motors:
-        # skip motors without motor_id or topic
-        if 'topic' not in motor or 'motor_id' not in motor or 'name' not in motor:
-            continue
-
-        # load calibration file if not loaded yet
-        if not motor['topic'] in calibration:
-            calibration_config = config_root + motor['topic'] + "_pololu.yaml"
-
-            if os.path.isfile(calibration_config):
-                calibration[motor['topic']] = read_yaml(calibration_config)
-
-        motor['calibration'] = calibration[motor['topic']][motor['name']]
-
+    motors = read_yaml(os.path.join(config_root, 'motors_settings.yaml'))
+    #return json_encode(motors)
     return json_encode(motors)
+
+def set_configs(motors):
+    configs = Configs()
+    configs.parseMotors(motors)
+    return configs
 
 
 @app.route('/motors/update/<robot_name>', methods=['POST'])
 def update_motors(robot_name):
-    motors = json.loads(request.get_data().decode('utf8'))
-
-    calibration = {}
-    for motor in motors:
-        if 'topic' in motor and 'motor_id' in motor:
-            if not motor['topic'] in calibration:
-                calibration[motor['topic']] = {}
-
-            if 'calibration' in motor:
-                calibration[motor['topic']][motor['name']] = motor['calibration']
-                del motor['calibration']
-            else:
-                calibration[motor['topic']][motor['name']] = {
-                    'pololu_id': 0,
-                    'motor_id': motor['motor_id'],
-                    'init': radians_to_pulse(motor['default']),
-                    'min': radians_to_pulse(motor['min']),
-                    'max': radians_to_pulse(motor['max']),
-                    'reverse': False,
-                    'calibration': {
-                        'min_pulse': radians_to_pulse(motor['min']),
-                        'max_pulse': radians_to_pulse(motor['max']),
-                        'min_angle': radians_to_degrees(motor['min']),
-                        'max_angle': radians_to_degrees(motor['max']),
-                    }
-                }
-
-    for pololu in calibration.keys():
-        file_name = '/catkin_ws/src/robots_config/' + robot_name + '/' + pololu + '_pololu.yaml'
-        write_yaml(file_name, calibration[pololu])
+    motors = json.loads(request.get_data())
 
     # write to motor config
-    file_name = '/catkin_ws/src/robots_config/' + robot_name + '/motors.yaml'
-    write_yaml(file_name, motors)
-
-    return json_encode(True)
+    try:
+        file_name = os.path.join(config_root, 'motors_settings.yaml')
+        c = set_configs(motors)
+        write_yaml(file_name, motors)
+        cfgs = {'motors': c.motors, 'pololu': c.pololu, 'dynamixel': c.dynamixel}
+    except Exception as e:
+        return json_encode({'error': str(e)})
+    return json_encode({'error': False})
 
 
 @app.route('/expressions/<robot_name>', methods=['GET'])
@@ -130,7 +96,7 @@ def write_yaml(file_name, data):
         pass
 
     f = open(file_name, 'w')
-    f.write(yaml.dump(data))
+    f.write(yaml.safe_dump(data, encoding='utf-8', allow_unicode=True))
     f.close()
 
 
