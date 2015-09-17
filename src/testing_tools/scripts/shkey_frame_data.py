62a7b1b64ae082168166d0bc5255ffacdb4d3b0c
Wenwei Huang <dut.hww@gmail.com>
2015-09-17 10:24:39 +0800

    Reformat the output

--- src/testing_tools/scripts/shkey_frame_data.py
+++ src/testing_tools/scripts/shkey_frame_data.py
@@ -11,15 +11,19 @@ import time
 
 msgs = defaultdict(list)
 
-for i in range(1, 201):
-    bpy.context.scene.frame_set(i)
+frame_size = 200
+for i in range(frame_size):
+    bpy.context.scene.frame_set(i+1)
     time.sleep(0.01)
     data=roscom.api.getFaceData()
     print("frame", i)
     for k, v in data.items():
         msgs[k].append(v)
 with open('shkey_frame_data.csv', 'w') as f:
-    for k, v in msgs.items():
-        f.write('{},{}\n'.format(k, ','.join(map(str, v))))
-print('write done')
+    header = sorted(msgs.keys())
+    f.write('%s\n' % ','.join(header))
+    for i in range(frame_size):
+        values = [msgs[k][i] for k in header]
+        f.write('%s\n' % ','.join(map(str, values)))
+    print('write done')
 
