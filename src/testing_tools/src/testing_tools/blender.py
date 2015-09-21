4267783e314ee55b47a6bce30c7c94f65af6e194
Wenwei Huang <dut.hww@gmail.com>
2015-09-21 15:47:43 +0800

    Blender utilities

---
+++ src/testing_tools/src/testing_tools/blender.py
@@ -0,0 +1,27 @@
+import rospy
+from blender_api_msgs.srv import SetParam, GetParam
+
+set_param = rospy.ServiceProxy('/blender_api/set_param', SetParam)
+get_param = rospy.ServiceProxy('/blender_api/get_param', GetParam)
+
+def set_alive(alive):
+    """
+    alive:
+        True, keep model alive
+        False, the opposite
+    """
+    set_param("bpy.context.scene['keepAlive']", str(alive))
+
+def set_command_listener_active(active):
+    """
+    active:
+        True, wm.command_listener is not publishing PAU message
+        False, the opposite
+    """
+    set_param("bpy.context.scene['commandListenerActive']", str(alive))
+
+def get_shape_keys():
+    from collections import OrderedDict
+    shape_keys = eval(get_param("self.getFaceData()").value).keys()
+    return shape_keys
+
