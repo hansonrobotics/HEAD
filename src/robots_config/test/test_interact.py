2c26467ce7d3c863e1c465635a42b552504972e7
Wenwei Huang <dut.hww@gmail.com>
2015-12-17 18:03:35 +0800

    Make sure the launched nodes will be cleared up

--- src/robots_config/test/test_interact.py
+++ src/robots_config/test/test_interact.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import unittest
+import atexit
 import os
 import rostopic
 import rospy
@@ -12,17 +13,21 @@ import subprocess
 
 CWD = os.path.abspath(os.path.dirname(__file__))
 
+robot_name = 'han'
+cmd = 'roslaunch {}/launch/robot.launch basedir:={basedir} name:={name}'.format(
+    CWD, basedir='launch', name=robot_name)
+proc = subprocess.Popen(cmd, shell=True, preexec_fn=os.setsid)
+def shutdown():
+    if proc:
+        os.killpg(proc.pid, 2)
+atexit.register(shutdown)
+
 class InteractTest(unittest.TestCase):
 
     @classmethod
     def setUpClass(self):
-        self.robot_name = 'han'
-        launch_file = '{}/launch/robot.launch'.format(CWD)
-        cmd = 'roslaunch {} basedir:={basedir} name:={name}'.format(
-            launch_file, basedir='launch', name=self.robot_name)
-        self.proc = subprocess.Popen(cmd, shell=True, preexec_fn=os.setsid)
         for node in ['chatbot_en', 'chatbot_zh', 'speech2command', 'tts']:
-            wait_for(node, self.robot_name)
+            wait_for(node, robot_name)
         wait_for('blender_api')
         rospy.wait_for_service('/blender_api/set_param')
 
@@ -34,12 +39,7 @@ class InteractTest(unittest.TestCase):
             os.makedirs(self.output_audio)
         self.queue = MessageQueue()
         self.queue.subscribe(
-            '/{}/chatbot_responses'.format(self.robot_name), String)
-
-    @classmethod
-    def tearDownClass(self):
-        if self.proc:
-            os.killpg(self.proc.pid, 2)
+            '/{}/chatbot_responses'.format(robot_name), String)
 
     def setUp(self):
         set_alive(False)
@@ -52,7 +52,7 @@ class InteractTest(unittest.TestCase):
     def test_emotion_cmd(self):
         duration = 6
         pub, msg_class = rostopic.create_publisher(
-            '/{}/speech'.format(self.robot_name), 'chatbot/ChatMessage', True)
+            '/{}/speech'.format(robot_name), 'chatbot/ChatMessage', True)
         for cmd in ['sad', 'happy']:
             screen_output = '%s/bl_%s.avi' % (
                 self.output_video, cmd.replace(' ', '_'))
@@ -63,7 +63,7 @@ class InteractTest(unittest.TestCase):
     def test_gesture_cmd(self):
         duration = 2
         pub, msg_class = rostopic.create_publisher(
-            '/{}/speech'.format(self.robot_name), 'chatbot/ChatMessage', True)
+            '/{}/speech'.format(robot_name), 'chatbot/ChatMessage', True)
         for cmd in ['blink', 'nod', 'shake']:
             screen_output = '%s/bl_%s.avi' % (
                 self.output_video, cmd.replace(' ', '_'))
@@ -74,7 +74,7 @@ class InteractTest(unittest.TestCase):
     def test_turn_eye_cmd(self):
         duration = 2
         pub, msg_class = rostopic.create_publisher(
-            '/{}/speech'.format(self.robot_name), 'chatbot/ChatMessage', True)
+            '/{}/speech'.format(robot_name), 'chatbot/ChatMessage', True)
         pub.publish(msg_class('look center', 100))
         rospy.sleep(duration)
         for cmd in ['look left', 'look right']:
@@ -89,7 +89,7 @@ class InteractTest(unittest.TestCase):
     def test_turn_head_cmd(self):
         duration = 2
         pub, msg_class = rostopic.create_publisher(
-            '/{}/speech'.format(self.robot_name), 'chatbot/ChatMessage', True)
+            '/{}/speech'.format(robot_name), 'chatbot/ChatMessage', True)
         pub.publish(msg_class('turn center', 100))
         rospy.sleep(duration)
         for cmd in ['turn left', 'turn right']:
@@ -103,7 +103,7 @@ class InteractTest(unittest.TestCase):
 
     def test_chat(self):
         pub, msg_class = rostopic.create_publisher(
-            '/{}/speech'.format(self.robot_name), 'chatbot/ChatMessage', True)
+            '/{}/speech'.format(robot_name), 'chatbot/ChatMessage', True)
         self.queue.clear()
         for speech in ['hi', 'what\'s your name']:
             pub.publish(msg_class(speech, 100))
