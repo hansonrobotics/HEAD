adb24e12d8b12c8733588feb1a99b0e08ee9d9e4
tesyolan <tesfa1993@yahoo.com>
2016-01-09 17:49:26 +0300

    Include head and eye rotation from faceshift.

--- src/faceshift_puppeteering/src/puppeteering_node.cpp
+++ src/faceshift_puppeteering/src/puppeteering_node.cpp
@@ -6,9 +6,13 @@
 #include <ros/ros.h>
 #include <ros/package.h>
 
+#include <std_msgs/Bool.h>
 #include <blender_api_msgs/FSShapekey.h>
 #include <blender_api_msgs/FSShapekeys.h>
+#include <blender_api_msgs/FSValues.h>
 #include <blender_api_msgs/AnimationMode.h>
+#include <geometry_msgs/Pose.h>
+#include <geometry_msgs/Vector3.h>
 
 #include "fsbinarystream.h"
 using boost::asio::ip::tcp;
@@ -17,17 +21,28 @@ int main(int argc, char* argv[]) {
   bool not_connected = true;
   bool firstrun = true;
 
+  //Include values for the head pose.
+  geometry_msgs::Vector3 eye_left;
+  geometry_msgs::Vector3 eye_right;
+  geometry_msgs::Pose head_pose;
   blender_api_msgs::FSShapekeys shapekey_pairs;
+
+  //Publish on this message
+  blender_api_msgs::FSValues fs_value;
   fs::fsBinaryStream parserIn, parserOut;
   fs::fsMsgPtr msg;
 
+  //Parameters related to accessing socket
   boost::asio::io_service io_service;
   tcp::resolver resolver(io_service);
   tcp::socket socket(io_service);
 
+  //Parameters for message received and socket configuration.
   size_t len;
   std::string ip_num;
   std::string port_num;
+
+  //Node initalilzation and the socket handler.
   ros::init(argc, argv, "faceshift_to_ros");
   ros::NodeHandle nh;
   ros::Rate r(0.1);
@@ -37,8 +52,9 @@ int main(int argc, char* argv[]) {
   ros::Publisher pub = nh.advertise<blender_api_msgs::AnimationMode>(
       "/blender_api/set_animation_mode", 30, true);
 
-  ros::Publisher pub_shape = nh.advertise<blender_api_msgs::FSShapekeys>(
-      "/blender_api/faceshift_blendshapes_values", 30);
+  //Publish the message recieved from the ROS interface to the system.
+  ros::Publisher pub_shape = nh.advertise<blender_api_msgs::FSValues>(
+      "/blender_api/faceshift_values", 30);
   try {
     std::vector<std::string> blendshape_names;
 
@@ -122,15 +138,34 @@ int main(int argc, char* argv[]) {
           // printf ("Time: %f \n", data.m_timestamp);
           // printf ("Tracking Results: %s", data.m_trackingSuccessful ? "true"
           // : "false");
+          fs_value.tracking_status.data= data.m_trackingSuccessful;
+
+          fs_value.head_pose.position.x= data.m_headTranslation.x;
+          fs_value.head_pose.position.y= data.m_headTranslation.y;
+          fs_value.head_pose.position.z= data.m_headTranslation.z;
+
+          fs_value.head_pose.orientation.x = data.m_headRotation.x;
+          fs_value.head_pose.orientation.y = data.m_headRotation.y;
+          fs_value.head_pose.orientation.z = data.m_headRotation.z;
+          fs_value.head_pose.orientation.w = data.m_headRotation.w;
+
           // printf ("head translation: %f %f %f\n", data.m_headTranslation.x,
           // data.m_headTranslation.y, data.m_headTranslation.z);
           // ROS_INFO ("head rotation: %f %f %f %f \n", data.m_headRotation.x,
           // data.m_headRotation.y, data.m_headRotation.z,
           // data.m_headRotation.w);
+
+          fs_value.eye_left.x= data.m_eyeGazeLeftPitch;
+          fs_value.eye_left.y= data.m_eyeGazeLeftYaw;
           // printf ("Eye Gaze Left Pitch: %f\n", data.m_eyeGazeLeftPitch);
-          // printf ("Eye Gaze Left Pitch: %f\n", data.m_eyeGazeLeftYaw);
-          // printf ("Eye Gaze Left Pitch: %f\n", data.m_eyeGazeRightPitch);
-          // printf ("Eye Gaze Left Pitch: %f\n", data.m_eyeGazeRightYaw);
+          // printf ("Eye Gaze Left Yaw: %f\n", data.m_eyeGazeLeftYaw);
+
+
+          fs_value.eye_right.x= data.m_eyeGazeRightPitch;
+          fs_value.eye_right.y= data.m_eyeGazeRightYaw;
+
+          // printf ("Eye Gaze Right Pitch: %f\n", data.m_eyeGazeRightPitch);
+          // printf ("Eye Gaze Right Yaw: %f\n", data.m_eyeGazeRightYaw);
 
           std::vector<float> blend_shape = data.m_coeffs;
 
@@ -143,12 +178,16 @@ int main(int argc, char* argv[]) {
               blender_api_msgs::FSShapekey skey;
               skey.name = blendshape_names[counter];
               skey.value = *i;
-              shapekey_pairs.shapekey.push_back(skey);
+              fs_value.keys.shapekey.push_back(skey);
               // printf("Blendshape %s : %f\n",
               // blendshape_names[counter].c_str(),  *i);
               counter++;
             }
-            pub_shape.publish(shapekey_pairs);
+
+
+            pub_shape.publish(fs_value);
+            fs_value.keys.shapekey.clear();
+            //pub_shape.publish(shapekey_pairs);
           } else {
             firstrun = true;  // Cause the number of blendshapes and the rigging
                               // data have now changed in the system.
