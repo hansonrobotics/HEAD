14325c8ab64687521593bfe882251ff57d3b4c92
Henrikas Elsbergas <henrikas@hansonrobotics.com>
2016-02-21 16:47:36 +0000

    Adding slider extension to JsonEditor. Using sliders for node settings

---
+++ src/webui/public/js/lib/json_editor/slider.js
@@ -0,0 +1,20 @@
+define(['jquery', 'json_editor'], function ($, JSONEditor) {
+    JSONEditor.defaults.editors.slider = JSONEditor.defaults.editors.number.extend({
+        build: function () {
+            JSONEditor.defaults.editors.number.prototype.build.call(this);
+            var self = this,
+                slider = $('<div>').slider({
+                range: "min",
+                min: this.options.schema.minimum || 0,
+                max: this.options.schema.maximum || 100,
+                step: this.options.schema.step || 0.1,
+                value: this.value,
+                slide: function (e, ui) {
+                    $(self.input).val(ui.value);
+                }
+            });
+
+            $(this.input).after(slider);
+        }
+    });
+});
