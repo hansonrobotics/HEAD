be04973648c1ca07be430707a383e77b6aa4ca44
Henrikas Elsbergas <henrikas@hansonrobotics.com>
2016-02-28 11:36:32 +0000

    Adding dragon mode select button Sending chat input to chatbot_text

--- scripts/text_listener.py
+++ scripts/text_listener.py
@@ -17,24 +17,27 @@ class TextListener(object):
     def __init__(self):
         rospy.Subscriber('chatbot_text', String, self._cb)
         self.pub = rospy.Publisher('speech', ChatMessage, queue_size=1)
-        self.text_cache = []
+        self.text_cache = ''
         self.update_time = None
         self.timeout = rospy.get_param('timeout', 2)
 
     def _cb(self, msg):
         self.update_time = time.time()
-        self.text_cache.append(msg.data)
+        self.text_cache += msg.data
         threading.Timer(self.timeout, self.send).start()
         logger.info('Length {}, Append {}'.format(len(self.text_cache), msg.data))
 
     def send(self):
-        if time.time() - self.update_time >= self.timeout:
+        dragon_enabled = rospy.get_param('webui/speech_recognition') == 'dragon'
+        text = self.text_cache.strip()
+
+        if text and dragon_enabled and time.time() - self.update_time >= self.timeout:
             message = ChatMessage()
-            message.utterance = ' '.join(self.text_cache)
+            message.utterance = text
             message.confidence = 100
             self.pub.publish(message)
-            self.text_cache = []
-    
+            self.text_cache = ''
+
 if __name__ == '__main__':
     rospy.init_node('text_listener')
     TextListener()
