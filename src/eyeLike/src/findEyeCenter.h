dde1f5a29d6495158c906d259ec34ab99ae048b2
Wenwei Huang <dut.hww@gmail.com>
2016-03-01 10:50:28 +0800

    eyeLike is not going to integrate to eye tracking now.
    It's been pushed to a new repository: https://github.com/hansonrobotics/eyeLike.git

--- src/eyeLike/src/findEyeCenter.h
+++
@@ -1,8 +0,0 @@
-#ifndef EYE_CENTER_H
-#define EYE_CENTER_H
-
-#include "opencv2/imgproc/imgproc.hpp"
-
-cv::Point findEyeCenter(cv::Mat face, cv::Rect eye, std::string debugWindow);
-
-#endif
--- src/eyeLike/src/helpers.h
+++
@@ -1,9 +0,0 @@
-#ifndef HELPERS_H
-#define HELPERS_H
-
-bool rectInImage(cv::Rect rect, cv::Mat image);
-bool inMat(cv::Point p,int rows,int cols);
-cv::Mat matrixMagnitude(const cv::Mat &matX, const cv::Mat &matY);
-double computeDynamicThreshold(const cv::Mat &mat, double stdDevFactor);
-
-#endif
