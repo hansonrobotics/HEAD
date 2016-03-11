afe4d0295a896779be91bb62a392752ece3a5679
Wenwei Huang <dut.hww@gmail.com>
2016-03-11 09:38:55 +0800

    Reload arm

--- src/testing_tools/src/testing_tools/reload_motors.sh
+++ src/testing_tools/src/testing_tools/reload_motors.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
 
 rosnode kill /sophia_body/pololu_body
+sleep 1
 rosnode kill /sophia_body/pololu_head
-sleep 2
+sleep 1
+rosnode kill /sophia_body/pololu_arm
+sleep 1
 rosrun dynamic_reconfigure dynparam set /sophia_body/pau2motors reload True
