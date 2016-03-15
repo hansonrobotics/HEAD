2749cd62a253dbdec9d5a115612e43c9447fe8ed
Wenwei Huang <dut.hww@gmail.com>
2016-03-15 16:52:34 +0800

    Refactor

--- src/chatbot/src/server/character.py
+++ src/chatbot/src/server/character.py
@@ -4,10 +4,26 @@ import aiml
 class Character(object):
     def __init__(self, name):
         self.name = name
+        self.properties = {}
+
+    def get_properties(self):
+        return self.properties
+
+    def set_properties(self, props):
+        self.properties.update(props)
+
+    def respond(self, question, session=None):
+        return NotImplemented
+
+    def __repr__(self):
+        return "<Character name {}>".format(self.name)
+
+class AIMLCharacter(Character):
+    def __init__(self, name):
+        super(AIMLCharacter, self).__init__(name)
         self.character = aiml.Kernel()
         self.aiml_files = []
         self.character.verbose(False)
-        self.properties = {}
 
     def load_aiml_files(self, aiml_files):
         for f in aiml_files:
@@ -31,12 +47,9 @@ class Character(object):
             print >>sys.stderr, "Couldn't open {}".format(propname)
 
     def set_properties(self, props):
-        for key, value in props.iteritems():
+        super(AIMLCharacter, self).set_properties(props)
+        for key, value in self.properties.iteritems():
             self.character.setBotPredicate(key, value)
-            self.properties[key] = value
-
-    def get_properties(self):
-        return self.properties
 
     def respond(self, question, session=None):
         ret = {}
@@ -44,7 +57,4 @@ class Character(object):
         ret['emotion'] = self.character.getPredicate('emotion', session)
         ret['botname'] = self.name
         return ret
-
-    def __repr__(self):
-        return "<Character name {}>".format(self.name)
 
--- src/chatbot/src/server/characters/aiml_characters.py
+++ src/chatbot/src/server/characters/aiml_characters.py
@@ -1,22 +1,22 @@
 import os
-from server.character import Character
+from server.character import AIMLCharacter
 
 CWD = os.path.dirname(os.path.realpath(__file__))
 
-sophia = Character('sophia')
+sophia = AIMLCharacter('sophia')
 sophia.set_property_file(os.path.join(CWD, "../../../character_aiml/sophia.properties"))
 
-han = Character('han')
+han = AIMLCharacter('han')
 han.set_property_file(os.path.join(CWD, "../../../character_aiml/han.properties"))
 
-pkd = Character('pkd')
+pkd = AIMLCharacter('pkd')
 pkd.load_aiml_files([
     os.path.join(CWD, "../../../character_aiml/pkd.*.xml"),
     os.path.join(CWD, "../../../character_aiml/pkd.*.aiml")
 ])
 pkd.set_property_file(os.path.join(CWD, "../../../character_aiml/pkd.properties"))
 
-futurist_sophia = Character('futurist_sophia')
+futurist_sophia = AIMLCharacter('futurist_sophia')
 futurist_sophia.load_aiml_files([
     os.path.join(CWD, "../../../character_aiml/sophia.*.xml"),
     os.path.join(CWD, "../../../character_aiml/sophia.*.aiml")
