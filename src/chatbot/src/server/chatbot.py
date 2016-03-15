403749d55fea05e4f0e5a74c19e10e23d988f3b1
Wenwei Huang <dut.hww@gmail.com>
2016-03-15 16:53:09 +0800

    Improve question with SOLR

--- src/chatbot/src/server/chatbot.py
+++ src/chatbot/src/server/chatbot.py
@@ -1,12 +1,37 @@
 from characters import CHARACTERS
+from httplib import HTTPConnection
+import urllib
+import json
+import logging
 
 SUCCESS=0
 WRONG_CHARACTER_NAME=1
+
+logger = logging.getLogger('hr.chatbot.server.chatbot')
 
 def get_character(name):
     for character in CHARACTERS:
         if character.name == name:
             return character
+
+def solr(text):
+    # No match, try improving with SOLR
+    conn = HTTPConnection('localhost', 8983)
+    headers = {'Content-type': 'application/json'}
+    url = '/solr/aiml/select?indent=true&wt=json&fl=*,score&rows=20&qf=title&q=' + text.replace(' ', '%20')
+    conn.request('GET', url)
+    lucResponse = conn.getresponse()
+    conn.close()
+    # logger.warn('STATUS: %i', lucResponse.status)
+    lucText = lucResponse.read()
+
+    if len(lucText)>0:
+        logger.warn('RESPONSE: ' + lucText)
+        jResp = json.loads(lucText)
+        if jResp['response']['numFound'] > 0:
+            doc = jResp['response']['docs'][0]
+            lucResult = doc['title'][0]
+            return lucResult
 
 def ask(name, question, session=None):
     """
@@ -15,14 +40,27 @@ def ask(name, question, session=None):
     character = get_character(name)
     if not character:
         return None, WRONG_CHARACTER_NAME
+
     answer = character.respond(question, session)
     if answer.get('response', None):
         return answer, SUCCESS
-    else:
-        generic = get_character('generic')
-        generic.set_properties(character.get_properties())
-        answer = generic.respond(question, session)
-        return answer, SUCCESS
+
+    lucResult = None
+    try:
+        lucResult = solr(question)
+    except Exception as ex:
+        logger.warn(ex)
+
+    if lucResult:
+        answer = character.respond(lucResult)
+        logger.warn('LUCENE: %s -> %s' % (lucResult, answer))
+        if answer.get('response', None):
+            return answer, SUCCESS
+
+    generic = get_character('generic')
+    generic.set_properties(character.get_properties())
+    answer = generic.respond(question, session)
+    return answer, SUCCESS
 
 if __name__ == '__main__':
     for character in CHARACTERS:
