ca573344e5d4726752854330598380c9db7c9720
Wenwei Huang <dut.hww@gmail.com>
2016-03-21 12:33:36 +0800

    Add quant Han

--- src/chatbot/src/server/characters/aiml_characters.py
+++ src/chatbot/src/server/characters/aiml_characters.py
@@ -1,7 +1,30 @@
 import os
+import yaml
+import logging
 from server.character import AIMLCharacter
 
 CWD = os.path.dirname(os.path.realpath(__file__))
+
+logger = logging.getLogger('hr.chatbot.characters.aiml_characters')
+
+def load_aiml_character(character_yaml):
+    def abs_path(p):
+        if p.startswith('/'):
+            return p
+        return os.path.join(root_dir, p)
+
+    with open(character_yaml) as f:
+        spec = yaml.load(f)
+        try:
+            root_dir = os.path.dirname(os.path.realpath(character_yaml))
+            character = AIMLCharacter(spec['name'])
+            character.set_property_file(abs_path(spec['property_file']))
+            aiml_files = [abs_path(f) for f in spec['aiml']]
+            character.load_aiml_files(aiml_files)
+        except KeyError as ex:
+            logger.error(ex)
+
+    return character
 
 sophia = AIMLCharacter('sophia')
 sophia.set_property_file(os.path.join(CWD, "../../../character_aiml/sophia.properties"))
@@ -16,12 +39,6 @@ pkd.load_aiml_files([
 ])
 pkd.set_property_file(os.path.join(CWD, "../../../character_aiml/pkd.properties"))
 
-futurist_sophia = AIMLCharacter('futurist_sophia')
-futurist_sophia.load_aiml_files([
-    os.path.join(CWD, "../../../character_aiml/sophia.*.xml"),
-    os.path.join(CWD, "../../../character_aiml/sophia.*.aiml")
-])
-futurist_sophia.set_property_file(os.path.join(CWD, "../../../character_aiml/sophia.properties"))
+characters = [han, sophia, pkd]
 
-characters = [han, sophia, pkd, futurist_sophia]
-
+characters.append(load_aiml_character(os.path.join(CWD, 'quant_han.yaml')))
--- src/chatbot/src/server/run.py
+++ src/chatbot/src/server/run.py
@@ -3,7 +3,6 @@
 import os
 import sys
 from flask import Flask, request, Response
-from werkzeug.utils import secure_filename
 import json
 import logging
 from chatbot import ask
