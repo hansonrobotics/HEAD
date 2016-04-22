4343f491fcfdf3acef1d1abfc44be457c28361d6
Wenwei Huang <dut.hww@gmail.com>
2016-04-22 03:36:35 +0000

    Question flow current character -> local character -> generic character

--- src/chatbot/src/server/chatbot.py
+++ src/chatbot/src/server/chatbot.py
@@ -31,8 +31,17 @@ def get_character(id, create=False):
         logger.info("Create SheetAIMLCharacter {}".format(character))
         return character
 
-def get_characters_by_name(name):
-    return [c for c in CHARACTERS if c.name == name]
+def is_local_character(character):
+    if isinstance(character, server.character.SheetAIMLCharacter) or \
+            isinstance(character, SheetAIMLCharacter):
+        return False
+    return True
+
+def get_characters_by_name(name, local=True):
+    characters = [c for c in CHARACTERS if c.name == name]
+    if local:
+        characters = [c for c in characters if is_local_character(c)]
+    return characters
 
 def list_character():
     return [c.id for c in CHARACTERS]
@@ -126,13 +135,12 @@ def ask(id, question, session=None):
     if not character:
         return response, WRONG_CHARACTER_NAME
 
-    # current character > character with the same name > generic character
-    responding_characters = get_characters_by_name(character.name)
-    responding_characters.remove(character)
+    # current character > local character with the same name > generic character
+    responding_characters = get_characters_by_name(character.name, local=True)
+    if character in responding_characters:
+        responding_characters.remove(character)
     responding_characters = sorted(responding_characters, key=lambda x: x.level)
     responding_characters.insert(0, character)
-    generic = get_character('generic')
-    generic.set_properties(character.get_properties())
     logger.info("Responding characters {}".format(responding_characters))
 
     _response = _ask_characters(responding_characters, question, session)
@@ -148,7 +156,10 @@ def ask(id, question, session=None):
             if _response is not None:
                 _response['solr'] = lucResult
         else:
-            _response = _ask_characters([generic], question, session)
+            generic = get_character('generic')
+            if generic:
+                generic.set_properties(character.get_properties())
+                _response = _ask_characters([generic], question, session)
 
     if _response is not None:
         response.update(_response)
