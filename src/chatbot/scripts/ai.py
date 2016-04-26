835f0a83e97777bc567539da62842730fbd45b8f
Wenwei Huang <dut.hww@gmail.com>
2016-04-26 19:39:25 +0800

    Refine clean cache

--- src/chatbot/scripts/ai.py
+++ src/chatbot/scripts/ai.py
@@ -22,6 +22,7 @@ class Chatbot():
   def __init__(self):
     self.chatbot_url = 'http://localhost:8001'
     self.botid = ''
+    self.clean_cache()
 
     # chatbot now saves a bit of simple state to handle sentiment analysis
     # after formulating a response it saves it in a buffer if S.A. active
@@ -178,19 +179,27 @@ class Chatbot():
     self._echo_publisher.publish(message)
 
   def clean_cache(self):
-      params = {
-          "botid": "{}".format(self.botid),
-          "Auth": key
-      }
-      r = requests.get('{}/{}/clean_cache'.format(self.chatbot_url, VERSION),
-                        params=params)
-      logger.info("Server cache is cleaned. {}".format(r.json()))
+    lang = rospy.get_param('lang', None)
+    if lang == 'zh':
+      self.set_botid(rospy.get_param('botid_zh', None))
+    elif lang == 'en':
+      self.set_botid(rospy.get_param('botid_en', None))
+    else:
+      logger.warn('Language {} is not supported'.format(lang))
+      return
+    params = {
+        "botid": "{}".format(self.botid),
+        "Auth": key
+    }
+    r = requests.get('{}/{}/clean_cache'.format(self.chatbot_url, VERSION),
+                      params=params)
+    logger.info("Clean server cache {}".format(r.json()))
 
   def reconfig(self, config, level):
     self.sentiment_active(config.sentiment)
     if self.chatbot_url != config.chatbot_url:
-        self.chatbot_url = config.chatbot_url
-        self.clean_cache()
+      self.chatbot_url = config.chatbot_url
+      self.clean_cache()
     return config
 
 if __name__ == '__main__':
