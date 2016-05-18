b1dd684f7679641d9001a9e29189116b25e7b2f5
tesyolan <tesfa1993@yaoo.com>
2016-05-18 16:45:15 +0800

    Install tools and include message.

---
+++ scripts/vision.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+#TODO launch the different variant of vision_tools in this repository along with RVIZ mapper in the system.
+
+#TODO CMT and EMotime with openface
+
+#TODO Pi Tracker in the system.
