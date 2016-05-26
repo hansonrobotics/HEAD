7423381844b03cecc4ec99cee2c4130cf1bebf04
Wenwei Huang <dut.hww@gmail.com>
2016-05-26 11:06:06 +0800

    Set required environment variables

--- scripts/vision.sh
+++ scripts/vision.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 #Pi_VISION
+
+export BASEDIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+eval $($BASEDIR/hrtool -p|grep -E ^HR_WORKSPACE=)
+export HR_WORKSPACE=$HR_WORKSPACE
+
+source $HR_WORKSPACE/HEAD/devel/setup.bash
+export PYTHONPATH=$PYTHONPATH:$HR_WORKSPACE/openface/:$HR_WORKSPACE/dlib-18.18/dist/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HR_WORKSPACE/CppMT/:$HR_WORKSPACE/emotime/build/src
+
 export tool="$1"
 if [[ -z $tool ]]; then
     echo "Using Pi Vision"
