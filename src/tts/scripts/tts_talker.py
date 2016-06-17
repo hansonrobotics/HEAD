410b506fb86b623854eeba7942f7fb13ad6cf5d8
Wenwei Huang <dut.hww@gmail.com>
2016-06-17 18:07:22 +0800

    Refactoring: be able to use different TTS engine

--- src/tts/scripts/tts_talker.py
+++ src/tts/scripts/tts_talker.py
@@ -3,112 +3,65 @@
 
 import rospy
 import time
-import re
 import logging
 import os
-import sys
-import random
-import threading
-import pyglet
-import subprocess
 
 import SoundFile
-from basic_head_api.msg import MakeFaceExpr
 from std_msgs.msg import String
 from tts.srv import TTSLengthResponse
-from topic_tools.srv import MuxSelect
 from blender_api_msgs.msg import Viseme
-from visemes import BaseVisemes, English_Visemes
+from common.visemes import BaseVisemes
+from ttsapi import get_api
 
 logger = logging.getLogger('hr.tts.tts_talker')
-
-#CWD = os.path.dirname(os.path.realpath(__file__))
-#sys.path.insert(0, os.path.join(CWD, '../pyfestival'))
-#import festival
 
 class TTSTalker:
     def __init__(self):
         topic = rospy.get_param('~topic_name', 'tts')
         rospy.Subscriber(topic, String, self.say)
-        rospy.Subscriber(topic+'_en', String, self.say)
+        rospy.Subscriber(topic+'_en', String, self.say, 'en')
         self.speech_active = rospy.Publisher('speech_events', String, queue_size=10)
         self.vis_topic = rospy.Publisher('/blender_api/queue_viseme', Viseme, queue_size=0)
-        self.lipsync_enabled = rospy.get_param('lipsync', True)
-        self.lipsync_blender = rospy.get_param('lipsync_blender', True)
-        self.mux = rospy.ServiceProxy('lips_pau_select', MuxSelect)
-        self.expr_topic = rospy.Publisher('make_face_expr', MakeFaceExpr, queue_size=0)
-        self.lipsync = False
+        self.tts_api_config = rospy.get_param('tts_api_config', {'en': 'festival'})
         self.sound = SoundFile.SoundFile()
-        self.output_dir = os.path.expanduser('~/.hr/tts/festival')
-        if not os.path.isdir(self.output_dir):
-            os.makedirs(self.output_dir)
-        self.wavout = os.path.join(self.output_dir, 'tts.wav')
-        self.timing = os.path.join(self.output_dir, 'timing')
-        self.script = os.path.join(self.output_dir, 'tts.scm')
-        self.viseme_config = English_Visemes()
+        self.tts_data = None
 
     def tts_length(self, req):
         return TTSLengthResponse(1)
 
-    def say(self, msg):
+    def say(self, msg, lang=None):
+        if lang is None:
+            lang = rospy.get_param('lang', None)
+            if lang is None:
+                logger.error("Language is not set")
+                return
         text = msg.data
         self.startLipSync()
-        self._say(text)
+        self._say(text, lang)
         self.stopLipSync()
         logger.info("Finished tts")
 
-    def _say(self, text):
-        try:
-            logger.info('Say "{}"'.format(text))
-            with open(self.script, 'w') as f:
-                f.write("""
-(voice_cmu_us_slt_arctic_hts)
-(set! utt1 (Utterance Text "{text}"))
-(utt.synth utt1)
-(utt.save.segs utt1 "{timingfile}")
-(utt.save.wave utt1 "{audiofile}")""".format(
-                    text=text, timingfile=self.timing, audiofile=self.wavout)
-                )
-            subprocess.Popen(
-                ['festival', '-b', self.script], stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE).communicate()
-        except Exception as ex:
-            import traceback
-            logger.error('TTS error: {}'.format(traceback.format_exc()))
+    def _say(self, text, lang):
+        logger.info('Say "{}" in {}'.format(text, lang))
+        api_name = self.tts_api_config.get(lang, None)
+        api = get_api(api_name)
+        if api is None:
+            logger.error("No TTS API")
             return
-        self.doLipSync()
-        os.remove(self.wavout)
-        os.remove(self.timing)
-        os.remove(self.script)
+        self.tts_data = api.tts(text)
+        if self.tts_data and hasattr(self.tts_data, "visemes"):
+            self.doLipSync()
 
     def startLipSync(self):
-        self.lipsync = True
         self.speech_active.publish("start")
-        if self.lipsync_enabled and not self.lipsync_blender:
-            self.mux("lipsync_pau")
 
     def stopLipSync(self):
-        self.lipsync = False
         self.speech_active.publish("stop")
-        if self.lipsync_enabled and not self.lipsync_blender:
-            self.mux("head_pau")
 
     def doLipSync(self):
-        self.sound.start(self.wavout)
+        self.sound.start(self.tts_data.wavout)
 
-        phonemes = []
-        with open(self.timing) as f:
-            lines = f.read().splitlines()
-            last_tick = 0
-            for line in lines:
-                if line.strip().startswith('#'):
-                    continue
-                tick, vol, phoneme  = line.strip().split(' ')
-                tick = float(tick)
-                phonemes.append({'name': phoneme, 'start': last_tick, 'end': tick})
-                last_tick = tick
-        visemes = self.viseme_config.get_visemes(phonemes)
-
+        visemes = self.tts_data.visemes
         start = time.time()
         i = 0
         while i < len(visemes):
@@ -120,7 +73,7 @@ class TTSTalker:
         self.sendVisime({'name': 'Sil'})
 
         elapsed_time = 0
-        timeout = 10
+        timeout = 0.5
         while self.sound.is_playing and elapsed_time < timeout:
             time.sleep(0.05)
             elapsed_time += 0.05
@@ -128,21 +81,20 @@ class TTSTalker:
         self.sound.stop()
 
     def sendVisime(self, visime):
-        if self.lipsync_enabled and self.lipsync_blender and (visime['name'] != 'Sil'):
-            #Need to have global shapekey_store class.
+        if visime['name'] != 'Sil':
             msg = Viseme()
-            # Duration should be overlapping
             msg.duration.nsecs = visime['duration']*1000000000*BaseVisemes.visemes_param[visime['name']]['duration']
             msg.name = visime['name']
             msg.magnitude = BaseVisemes.visemes_param[visime['name']]['magnitude']
             msg.rampin = BaseVisemes.visemes_param[visime['name']]['rampin']
             msg.rampout = BaseVisemes.visemes_param[visime['name']]['rampout']
             self.vis_topic.publish(msg)
-        if self.lipsync_enabled and not self.lipsync_blender:
-            msg = MakeFaceExpr()
-            msg.exprname = 'vis_'+visime['name']
-            msg.intensity = 1.0
-            self.expr_topic.publish(msg)
+        else:
+            # Send silence viseme: Using M instead
+            msg = Viseme()
+            msg.duration.nsecs = 1000000000
+            msg.name = 'M'
+            self.vis_topic.publish(msg)
 
 if __name__ == '__main__':
     rospy.init_node('tts_talker')
