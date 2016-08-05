f5e396da11e870293ed6dddc941c9409398209da
Wenwei Huang <dut.hww@gmail.com>
2016-08-05 19:08:25 +0800

    Add the path of chatbot package

--- scripts/slack_client.py
+++ scripts/slack_client.py
@@ -6,6 +6,9 @@ import time
 import logging
 import requests
 import re
+import sys
+CWD = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, os.path.join(CWD, '../src'))
 from chatbot.server.session import SessionManager
 
 VERSION = 'v1.1'
