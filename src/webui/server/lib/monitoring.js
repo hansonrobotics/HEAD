e44f74a7ab4035487433af595a9ecef8e28d6572
Henrikas Elsbergas <elsbergass@gmail.com>
2016-09-08 23:00:48 +0300

    Implementing monitoring functionality and command line options

--- src/webui/server/lib/monitoring.js
+++ src/webui/server/lib/monitoring.js
@@ -1,6 +1,8 @@
 var os = require('os'),
+    yaml = require('yamljs'),
     memory = require('./memory'),
-    cp = require('child_process');
+    cp = require('child_process'),
+    path = require('path');
 
 module.exports = {
     startStatus: function (config_dir, robot_name, started) {
@@ -9,8 +11,8 @@ module.exports = {
 
         // Check if software started only if its status wasnt changed on same call,
         // in which case current status would be passed as argument
-        //if started < 0
-        //  started = self.robot_started(robot_name)
+        if (started < 0)
+            started = this.robot_started(robot_name);
 
         var totalMem = memory.totalMemory(),
             usedMem = memory.usedMemory(),
@@ -19,23 +21,119 @@ module.exports = {
                 'system': {
                     'cpu': parseInt(os.loadavg()[0] / os.cpus().length * 100),
                     'mem': parseInt(usedMem / totalMem * 100),
-                    'total_mem': parseInt(totalMem)
+                    'total_mem': parseInt(totalMem),
+                    'fps': this.get_blender_fps()
                 },
-                //'status':
-                //  'internet': self.get_internet_status()
-                //  'camera': self.check("test -e /dev/video0") * -1 + 1
+                'status': {
+                    'internet': this.get_internet_status(),
+                    'ros': this.get_ros_status(),
+                    'blender': this.get_blender_status(),
+                    'pololu': this.check('test', ['-e', '/dev/ttyACM0']) * -1 + 1,
+                    'usb2dynamixel': this.check('test', ['-e', '/dev/ttyUSB0']) * -1 + 1,
+                    'camera': this.check('test', ['-e', '/dev/video0']) * -1 + 1
+                },
                 'checks': []
             };
-        console.log(this.get_blender_fps());
+
+        if (status['software'] == 0) {
+            var hw = yaml.load(path.join(config_dir, robot_name, 'hw_monitor.yaml'));
+            if ('dynamixel' in hw) {
+                for (let c of hw.dynamixel) {
+                    // Check device
+                    var dev = this.check("test", ['-e', c['device']]) * -1 + 1;
+                    status['checks'].push({
+                        'label': c['label'] + " USB",
+                        'status': dev,
+                    });
+
+                    // Check for motors if device found
+                    var found = 0;
+                    if (dev == 0)
+                        found = this.get_dynamixel_motor_number(c['device'])
+
+                    var total = 0;
+                    if ('motor_count' in c)
+                        total = c['motor_count'];
+                    // All motors found
+                    var st = 1;
+                    if (found >= total && total > 0)
+                        st = 0;
+                    else if (found > 0)
+                        st = 2;
+
+                    var val = str(found);
+                    if (total > 0)
+                        val += "/{}".format(total);
+
+                    status['checks'].append({
+                        'label': c['label'] + " Motors found",
+                        'status': st,
+                        'value': val,
+                    });
+                }
+            }
+        }
+
+        status['checks'].push({
+            'label': "Internet Connection",
+            'status': this.get_internet_status(),
+        });
+
         return status;
     },
     call_ros_service: function (service, args) {
         if (args.constructor !== Array)
             args = [];
 
-        return cp.spawnSync('rosservice', ['call', service].concat(args), { encoding : 'utf8' });
+        var res = cp.spawnSync('rosservice', ['call', service].concat(args), {encoding: 'utf8'});
+
+        if (res.status > 0)
+            return null;
+        else
+            return res.stdout;
     },
     get_blender_fps: function () {
-        return this.call_ros_service("/blender_api/get_param", ['"bpy.data.scenes[\'Scene\'].evaFPS"'])
+        var res = this.call_ros_service("/blender_api/get_param", ['"bpy.data.scenes[\'Scene\'].evaFPS"'])
+        if (res) {
+            res = yaml.parse(res);
+            if ('value' in res)
+                return res['value'];
+        }
+
+        return 0;
+    },
+    robot_started: function (name) {
+        var res = cp.spawnSync('tmux', ['list-sessions'], {encoding: 'utf8'});
+        if (res.status <= 0) {
+            var lines = res.stdout.split("\n");
+            for (let line of lines) {
+                if (line.indexOf(name) !== -1) {
+                    if (line.indexOf('attached') !== -1)
+                        return 1;
+                    else
+                        return 2;
+                }
+            }
+        }
+        return 0;
+    },
+    get_internet_status: function () {
+        return cp.spawnSync('ping', ['8.8.8.8', '-c', '1', '-W', '1'], {encoding: 'utf8'}).status
+    },
+    get_ros_status: function () {
+        return cp.spawnSync('pgrep', ['roscore'], {encoding: 'utf8'}).status
+    },
+    get_blender_status: function () {
+        return cp.spawnSync('pgrep', ['blender'], {encoding: 'utf8'}).status;
+    },
+    check: function (cmd, args) {
+        return cp.spawnSync(cmd, args, {encoding: 'utf8'}).status === 0;
+    },
+    get_dynamixel_motor_number: function(device) {
+        if (self.mx_tool) {
+            var res = cp.spawnSync(self.mx_tool, ['--device', device], {encoding: 'utf8'});
+            if (res.status <= 0) return res.stdout.split("\n").length - 1;
+        }
+        return 0;
     }
 };
--- src/webui/server/server.js
+++ src/webui/server/server.js
@@ -1,22 +1,55 @@
 var express = require('express'),
-  app = express(),
-  config = require('./config'),
-  monitoring = require('./lib/monitoring'),
-  path = require('path'),
-  yaml = require('yamljs');
+    app = express(),
+    config = require('./config'),
+    monitoring = require('./lib/monitoring'),
+    fs = require('fs'),
+    path = require('path'),
+    yaml = require('yamljs'),
+    argv = require('yargs')
+        .usage('Usage: $0 [options]')
+        .options({
+            r: {
+                alias: 'robot',
+                default: 'sophia',
+                describe: 'Robot name'
+            },
+            e: {
+                alias: 'exec',
+                describe: 'Robot execution script'
+            },
+            s: {
+                alias: 'stop',
+                describe: 'Robot stop script'
+            },
+            c: {
+                alias: 'config',
+                describe: 'Robot config dir'
+            },
+            p: {
+                alias: 'port',
+                type: 'number',
+                describe: 'Port',
+                default: 8003
+            },
+            m: {
+                alias: 'mx',
+                describe: 'Dynamixel motor command line tool'
+            }
+        }).help('h').alias('h', 'help').argv;
 
 app.use('/public', express.static('../public'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.resolve(__dirname + '/../public/index.html'));
+    res.sendFile(path.resolve(__dirname + '/../public/index.html'));
 });
 
 app.get('/robot_config.js', function (req, res) {
     var filename = path.resolve(config.configRoot, config.robot, 'webstart.js');
-    if (fs.existsSync(filename))
+    if (monitoring.robot_started(argv.robot) === 1 || !fs.existsSync(filename)) {
+        res.send("define(function (){return {mode:'normal'}});");
+    } else {
         res.sendFile(filename);
-    else
-        res.send('module.exports = {};')
+    }
 });
 
 app.get('/robot_config_schema', function (req, res) {
@@ -31,4 +64,4 @@ app.get('/monitor/start_status', function (req, res) {
     res.json(monitoring.startStatus(config.configRoot, config.robot));
 });
 
-app.listen(8003);
+app.listen(argv.port);
