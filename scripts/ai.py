e96e4ee258125a1f84c9cd83da48d8bf960e491e
Wenwei Huang <dut.hww@gmail.com>
2016-09-09 17:37:13 +0800

    Add delay response option to chatbot
    If enabled, it will wait for more inputs if there is any until it's timeout.

--- scripts/ai.py
+++ scripts/ai.py
@@ -6,6 +6,8 @@ import logging
 import requests
 import json
 import time
+import threading
+import re
 
 from chatbot.polarity import Polarity
 from chatbot.msg import ChatMessage
@@ -17,6 +19,8 @@ from chatbot.client import get_default_username
 logger = logging.getLogger('hr.chatbot.ai')
 VERSION = 'v1.1'
 key = 'AAAAB3NzaC'
+trace_pattern = re.compile(
+    r'../(?P<fname>.*), (?P<tloc>\(.*\)), (?P<pname>.*), (?P<ploc>\(.*\))')
 
 
 class Chatbot():
@@ -35,7 +39,7 @@ class Chatbot():
         # after formulating a response it saves it in a buffer if S.A. active
         # It has a simple state transition - initialized in wait_client
         # after getting client if S.A. active go to wait_emo
-        #  in affect_express call back publish response and reset to wait_client
+        # in affect_express call back publish response and reset to wait_client
         self._response_buffer = ''
         self._state = 'wait_client'
         # argumment must be  to activate sentiment analysis
@@ -45,9 +49,17 @@ class Chatbot():
         self._polarity_threshold = 0.2
         self.speech = False
 
+        self.input_stack = []
+        self.condition = threading.Condition()
+        self.respond_worker = threading.Thread(target=self.process_input)
+        self.respond_worker.daemon = True
+        self.respond_worker.start()
+        self.delay_response = rospy.get_param('delay_response', False)
+
         rospy.Subscriber('chatbot_speech', ChatMessage, self._request_callback)
         rospy.Subscriber('speech_events', String, self._speech_event_callback)
-        self.tts_ctrl_pub = rospy.Publisher('tts_control', String, queue_size=1)
+        self.tts_ctrl_pub = rospy.Publisher(
+            'tts_control', String, queue_size=1)
 
         self._response_publisher = rospy.Publisher(
             'chatbot_responses', String, queue_size=1)
@@ -67,6 +79,7 @@ class Chatbot():
         self._echo_publisher = rospy.Publisher(
             'perceived_text', String, queue_size=1)
         rospy.Subscriber('chatbot_speech', ChatMessage, self._echo_callback)
+        rospy.set_param('node_status/chatbot', 'running')
 
     def ping(self):
         try:
@@ -95,12 +108,13 @@ class Chatbot():
     def sentiment_active(self, active):
         self._sentiment_active = active
 
-    def get_response(self, question, lang):
+    def get_response(self, question, lang, query=False):
         params = {
             "question": "{}".format(question),
             "session": self.session,
             "lang": lang,
-            "Auth": key
+            "Auth": key,
+            "query": query,
         }
         r = requests.get('{}/{}/chat'.format(self.chatbot_url, VERSION),
                          params=params)
@@ -136,80 +150,106 @@ class Chatbot():
             self._affect_publisher.publish(String('sad'))
             return
 
-        lang = rospy.get_param('lang', None)
-
-        response = ''
-
-        blink = String()
         # blink that we heard something, request, probability defined in
         # callback
-        blink.data = 'chat_heard'
-        self._blink_publisher.publish(blink)
+        self._blink_publisher.publish('chat_heard')
 
         if chat_message.confidence < 50:
-            response = 'Could you say that again?'
-            message = String()
-            message.data = response
-            self._response_publisher.publish(message)
-            # puzzled expression
+            self._response_publisher.publish('Could you say that again?')
+            return
+
+        if self.delay_response:
+            with self.condition:
+                logger.info("Add input: {}".format(chat_message.utterance))
+                self.input_stack.append((time.clock(), chat_message))
+                self.condition.notify_all()
         else:
-            # request blink, probability of blink defined in callback
-            blink.data = 'chat_saying'
-            self._blink_publisher.publish(blink)
+            self.respond([chat_message.utterance])
 
-            try:
-                answer = self.get_response(chat_message.utterance, lang)
-            except Exception:
-                self.session = self.start_session()
-                answer = self.get_response(chat_message.utterance, lang)
+    def process_input(self):
+        while True:
+            with self.condition:
+                if not self.input_stack:
+                    continue
+                num_input = len(self.input_stack)
+                questions = [i[1].utterance for i in self.input_stack]
+                question = ' '.join(questions)
+                logger.info("Current input: {}".format(question))
+                if len(question) < 10:
+                    self._response_publisher.publish(String('uha'))
+                    self.condition.wait(2)
+                    if len(self.input_stack) > num_input:
+                        continue
+                self.respond(questions)
+                del self.input_stack[:]
+            time.sleep(0.1)
 
-            response = answer.get('text')
-            emotion = answer.get('emotion')
-            botid = answer.get('botid')
+    def respond(self, questions):
+        lang = rospy.get_param('lang', None)
+        for question in questions:
+            tmp_answer = self.get_response(question, lang, True)
+            traces = tmp_answer.get('trace')
+            if traces:
+                pattern = [trace_pattern.match(trace).group('pname')
+                           for trace in traces]
+                logger.info("Question {}, Pattern {}".format(
+                    question, ' '.join(pattern)))
 
-            # Add space after punctuation for multi-sentence responses
-            response = response.replace('?', '? ')
-            response = response.replace('.', '. ')
-            response = response.replace('_', ' ')
+        question = ' '.join(questions)
+        try:
+            answer = self.get_response(question, lang)
+        except Exception:
+            self.session = self.start_session()
+            answer = self.get_response(question, lang)
 
-            # if sentiment active save state and wait for affect_express to publish response
-            # otherwise publish and let tts handle it
-            if self._sentiment_active:
-                emo = String()
-                if emotion:
-                    emo.data = emotion
+        response = answer.get('text')
+        emotion = answer.get('emotion')
+        botid = answer.get('botid')
+
+        # Add space after punctuation for multi-sentence responses
+        response = response.replace('?', '? ')
+        response = response.replace('.', '. ')
+        response = response.replace('_', ' ')
+
+        # if sentiment active save state and wait for affect_express to publish response
+        # otherwise publish and let tts handle it
+        if self._sentiment_active:
+            emo = String()
+            if emotion:
+                emo.data = emotion
+                self._affect_publisher.publish(emo)
+                rospy.loginfo(
+                    '[#][PERCEIVE ACTION][EMOTION] {}'.format(emo.data))
+                logger.info('Chatbot perceived emo: {}'.format(emo.data))
+            else:
+                p = self.polarity.get_polarity(response)
+                logger.info('Polarity for "{}" is {}'.format(
+                    response.encode('utf-8'), p))
+                # change emotion if polarity magnitude exceeds threshold defined in constructor
+                # otherwise let top level behaviors control
+                if p > self._polarity_threshold:
+                    emo.data = 'happy'
                     self._affect_publisher.publish(emo)
                     rospy.loginfo(
                         '[#][PERCEIVE ACTION][EMOTION] {}'.format(emo.data))
-                    logger.info('Chatbot perceived emo: {}'.format(emo.data))
-                else:
-                    p = self.polarity.get_polarity(response)
-                    logger.info('Polarity for "{}" is {}'.format(
-                        response.encode('utf-8'), p))
-                    # change emotion if polarity magnitude exceeds threshold defined in constructor
-                    # otherwise let top level behaviors control
-                    if p > self._polarity_threshold:
-                        emo.data = 'happy'
-                        self._affect_publisher.publish(emo)
-                        rospy.loginfo(
-                            '[#][PERCEIVE ACTION][EMOTION] {}'.format(emo.data))
-                        logger.info(
-                            'Chatbot perceived emo: {}'.format(emo.data))
-                        # Currently response is independant of message received so no need to wait
-                        # Leave it for Opencog to handle responses later on.
-                    elif p < 0 and abs(p) > self._polarity_threshold:
-                        emo.data = 'frustrated'
-                        self._affect_publisher.publish(emo)
-                        rospy.loginfo(
-                            '[#][PERCEIVE ACTION][EMOTION] {}'.format(emo.data))
-                        logger.info(
-                            'Chatbot perceived emo: {}'.format(emo.data))
-                        # Currently response is independant of message received so no need to wait
-                        # Leave it for Opencog to handle responses later on.
+                    logger.info(
+                        'Chatbot perceived emo: {}'.format(emo.data))
+                    # Currently response is independant of message received so no need to wait
+                    # Leave it for Opencog to handle responses later on.
+                elif p < 0 and abs(p) > self._polarity_threshold:
+                    emo.data = 'frustrated'
+                    self._affect_publisher.publish(emo)
+                    rospy.loginfo(
+                        '[#][PERCEIVE ACTION][EMOTION] {}'.format(emo.data))
+                    logger.info(
+                        'Chatbot perceived emo: {}'.format(emo.data))
+                    # Currently response is independant of message received so no need to wait
+                    # Leave it for Opencog to handle responses later on.
 
-            self._response_publisher.publish(String(response))
-            logger.info("Ask: {}, answer: {}, answered by: {}".format(
-                chat_message.utterance, response.encode('utf-8'), botid))
+        self._blink_publisher.publish('chat_saying')
+        self._response_publisher.publish(String(response))
+        logger.info("Ask: {}, answer: {}, answered by: {}".format(
+            question, response.encode('utf-8'), botid))
 
     # Just repeat the chat message, as a plain string.
     def _echo_callback(self, chat_message):
@@ -223,6 +263,7 @@ class Chatbot():
             self.chatbot_url = config.chatbot_url
             self.session = self.start_session()
         self.enable = config.enable
+        self.delay_response = config.delay_response
         return config
 
 if __name__ == '__main__':
