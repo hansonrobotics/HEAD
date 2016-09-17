709e01182d0b281f242700747a73315a715b10af
natnaelargaw <natnaelargaw@gmail.com>
2016-09-17 08:38:33 +0300

    source code to publish normalized blib vals and mapping b/n dlib and blender shapekeys

---
+++ src/vision/facial_puppeteery/src/dlib_core.cpp
@@ -0,0 +1,253 @@
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <std_msgs/String.h>
+#include <std_msgs/UInt8.h>
+#include <math.h>
+
+#include <dlib/opencv.h>
+#include <dlib/image_processing/frontal_face_detector.h>
+#include <dlib/image_processing/render_face_detections.h>
+#include <dlib/image_processing.h>
+#include <dlib/gui_widgets.h>
+#include <unistd.h>
+#include <vector>
+#include <cstdlib>
+#include <facial_puppetry/land_marks.h>
+
+using namespace std;
+using namespace cv;
+using namespace dlib;
+
+//function declaration
+std::vector < float >  get_distance(double dP[70][2]);
+
+//holds dlib values including face TOP,LEFT, BOTTOM, RIGHT
+double FlandMark[70][2];
+char k;
+//holds relative LM's values: distance
+std::vector<float> distXY;
+//holds the maximum possible variation of LM's position
+std::vector<float> maxXY;
+
+//custom message that holds dlib values, maximum values, and face width and hight
+facial_puppetry::land_marks dlibRaw;
+
+
+dlib::image_window win;
+dlib::frontal_face_detector detector;
+dlib::shape_predictor pose_model;
+ros::Publisher pub;
+//ros::Publisher pub_anim_mode;
+
+
+// 1st pass normalization and shapekey mapping
+std::vector <float> get_distance(double dP[72][2])
+{
+// Optimization is Required Here: TBD
+distXY.push_back(dP[27][1] - (dP[20][1] +  dP[23][1])/2);// 0 . brow center upt
+maxXY.push_back((dP[27][1] - (dP[20][1] +  dP[23][1])/2)/2);
+
+distXY.push_back(0.0);   maxXY.push_back(1.0);  //brow center down
+
+distXY.push_back(dP[27][1] - dP[24][1]);//brow_inner_UP.L 2
+maxXY.push_back((dP[27][1] - dP[24][1])/3);
+
+distXY.push_back(0.0);   maxXY.push_back(1.0);//'brow_inner_DN.L
+
+distXY.push_back(dP[27][1] - dP[19][1]);//brow_inner_UP.R 4
+maxXY.push_back((dP[27][1] - dP[19][1])/3);
+
+distXY.push_back(0.0);   maxXY.push_back(1.0);//brow_inner_DN.R
+
+distXY.push_back(dP[27][1] - dP[24][1]);//brow_outer_UP.L 6
+maxXY.push_back((dP[27][1] - dP[24][1])/3);
+
+distXY.push_back(0.0);   maxXY.push_back(1.0);//brow_outer_DN.L
+
+distXY.push_back(dP[27][1] - dP[19][1]);//brow_outer_up.R 8
+maxXY.push_back((dP[27][1] - dP[19][1])/3);
+
+distXY.push_back(0.0);   maxXY.push_back(1.0);//brow_outer_DN.R
+
+distXY.push_back((dP[46][1]+dP[47][1])/2 - (dP[43][1]+dP[44][1])/2);//eye-flare.UP.L 10
+maxXY.push_back((dP[46][1]+dP[47][1])/2 - (dP[43][1]+dP[44][1])/2);
+
+
+distXY.push_back((1 / ((dP[46][1]+dP[47][1])/2 - (dP[43][1]+dP[44][1])/2))*100);//eye-blink.UP.L  11      ????
+maxXY.push_back(((dP[46][1]+dP[47][1])/2 - (dP[43][1]+dP[44][1])/2)*2.5);
+
+
+distXY.push_back(((dP[40][1]+dP[41][1])/2 - (dP[37][1]+dP[37][1])/2));//eye-flare.UP.R' 12
+maxXY.push_back((dP[40][1]+dP[41][1])/2 - (dP[37][1]+dP[38][1])/2);
+
+distXY.push_back((1 / ((dP[40][1]+dP[41][1])/2 - (dP[37][1]+dP[37][1])/2))*100);//eye-blink.UP.R 13    ???
+maxXY.push_back(((dP[40][1]+dP[41][1])/2 - (dP[37][1]+dP[38][1])/2)*2.5);
+
+
+distXY.push_back((1 / ((dP[46][1]+dP[47][1])/2 - (dP[43][1]+dP[44][1])/2))*100);//eye-blink.LO.L 14    ????
+maxXY.push_back(((dP[46][1]+dP[47][1])/2 - (dP[43][1]+dP[44][1])/2)*2.5);
+
+
+distXY.push_back((dP[46][1]+dP[47][1])/2 - (dP[43][1]+dP[44][1])/2);//eye-flare.LO.L 15
+maxXY.push_back(((dP[46][1]+dP[47][1])/2 - (dP[43][1]+dP[44][1])/2)*2);
+
+
+
+distXY.push_back((1 / ((dP[40][1]+dP[41][1])/2 - (dP[37][1]+dP[37][1])/2))*100);//eye-blink.LO.R 16     ????
+maxXY.push_back(((dP[40][1]+dP[41][1])/2 - (dP[37][1]+dP[38][1])/2)*2.5);
+
+
+distXY.push_back(((dP[40][1]+dP[41][1])/2 - (dP[37][1]+dP[37][1])/2));//eye-flare.LO.R   17
+maxXY.push_back(((dP[46][1]+dP[47][1])/2 - (dP[43][1]+dP[44][1])/2)*2);
+
+//hmmm, odd animation: jumped
+distXY.push_back(0.0);   maxXY.push_back(1.0); //wince.L         18
+distXY.push_back(0.0);   maxXY.push_back(1.0); //wince.R         19
+
+distXY.push_back((1/(dP[54][1] - dP[45][1])));//sneer.L     20
+maxXY.push_back(dP[33][1] - dP[29][1]);
+
+distXY.push_back((1/(dP[48][1] - dP[36][1])));//sneer.R     21
+maxXY.push_back(dP[33][1] - dP[29][1]);
+
+/*
+pass zero to unmapped shapekeys: eyes-look.dn   eyes-look.dn lip-UP.C.UP lip-UP.C.DN lip-UP.L.UP
+                                 lip-UP.L.DN  lip-UP.R.UP  lip-UP.R.DN
+*/
+
+for(int lm=0;lm<8;lm++){  distXY.push_back(0.0); maxXY.push_back(1.0);}
+
+
+distXY.push_back(dP[14][0] - dP[51][0]);//lips-smile.L 30    ????
+maxXY.push_back((dP[54][0] - dP[48][0])/2);
+
+distXY.push_back(dP[51][0] - dP[2][0]);//lips-smile.R  31     ???
+maxXY.push_back((dP[54][0] - dP[48][0])/2);
+
+distXY.push_back(dP[54][0]-dP[27][0]);//lips-wide.L 32
+maxXY.push_back((dP[54][0]-dP[27][0])/3);
+
+distXY.push_back(0.0);   maxXY.push_back(1.0);  //lips-narrow.L
+
+
+distXY.push_back(dP[27][0]-dP[48][0]);//lips-wide.R 34
+maxXY.push_back((dP[27][0]-dP[48][0])/3);
+
+
+/*
+pass zero to unmapped shapekeys: lips-narrow.R    lip-DN.C.DN     lip-DN.L.DN       lip-DN.L.UP
+                                 lip-DN.R.DN   lip-DN.R.UP'    lips-frown.L'   lips-frown.R
+*/
+for(int lm2=0;lm2<9;lm2++){  distXY.push_back(0.0); maxXY.push_back(1.0);}
+
+
+distXY.push_back((dP[65][1]+dP[66][1] +dP[67][1])/3 - (dP[61][1]+dP[62][1] +dP[63][1])/3);//lip-JAW.DN    ????? 44
+maxXY.push_back((dP[57][1] - dP[52][1])*2);
+
+return distXY;
+}
+
+
+//callback function
+void dlib_callback(const sensor_msgs::ImageConstPtr& msg) {
+//activate pau mode every time this func. is called: shall be handled in a shell script- TBD
+//pub_anim_mode.publish(16); //complete facial control
+
+//event handling
+//k=cvWaitKey(0);
+//if(k == 'A')
+//{
+//cout<<"Hello";
+//
+//}
+
+cv_bridge::CvImagePtr cvPtr;
+try
+{
+//ros image -> opencv image conversion
+cvPtr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+} catch (cv_bridge::Exception& e) {
+ROS_ERROR("cv_bridge exception: %s", e.what());
+return;
+}
+
+cv::Mat temp;
+temp = cvPtr->image;
+dlib::cv_image<dlib::bgr_pixel> cimg(temp);
+
+// Detect Faces
+std::vector<dlib::rectangle> faces = detector(cimg);
+
+// Find the pose of each face.
+std::vector<dlib::full_object_detection> shapes;
+
+//get pose
+for (unsigned long i = 0; i < faces.size(); ++i)
+{shapes.push_back(pose_model(cimg, faces[i]));}
+
+// if there is atleast a face
+if(shapes.size()>0)
+{
+for (unsigned long j = 0; j < shapes.size(); ++j) // Iterate through faces
+{
+const full_object_detection& d = shapes[j]; //get/store face
+
+for (unsigned long k = 0; k < 68; ++k) // get LMs
+{
+FlandMark[k][0] = (double)d.part(k).x();
+FlandMark[k][1] = (double)d.part(k).y();
+}
+}
+
+//get face coordinates: used to handle change in face position(in z dim: distance of ROI)
+//must find a way to handle Rigid motion (Face rotation on y axis)  TBD
+FlandMark[68][0] = faces[0].left();
+FlandMark[68][1] = faces[0].top();
+FlandMark[69][0] = faces[0].right();
+FlandMark[69][1] = faces[0].bottom();
+
+
+//assign values to custom msg attributes
+dlibRaw.dlib_val = get_distance(FlandMark); //relative distance
+dlibRaw.max_ref = maxXY; //maximum possible change per LM
+dlibRaw.distX = FlandMark[68][0]; //face Left
+dlibRaw.distY = FlandMark[68][1]; //face Top
+dlibRaw.distW = FlandMark[69][0]; //face width
+dlibRaw.distH = FlandMark[69][1]; //face bottom
+
+//publish Dlib Data
+pub.publish(dlibRaw);
+
+//Clear vcs for the upcoming computation
+distXY.clear();
+maxXY.clear();
+}
+
+//get each face data: will be used during DLIB improvement phase
+dlib::array<array2d<rgb_pixel> > face_chips;
+extract_image_chips(cimg, get_face_chip_details(shapes), face_chips);
+
+// Camera and Blender Viewer: get this directly inside blender UI or stream the 2d euivalent of 3D model into browser. TBD
+win.clear_overlay();
+win.set_image(cimg);
+win.add_overlay(render_face_detections(shapes));
+}
+
+
+//entry point
+int main(int argc, char **argv)
+{
+detector = dlib::get_frontal_face_detector();
+dlib::deserialize("/opt/hansonrobotics/vision/openface/models/dlib/shape_predictor_68_face_landmarks.dat") >> pose_model;
+ros::init(argc, argv, "dlib_core_node");
+ros::NodeHandle nh;
+pub = nh.advertise<facial_puppetry::land_marks>("/dlib_values", 1000);
+//pub_anim_mode = nh.advertise<std_msgs::UInt8>("/blender_api/set_animation_mode", 10);
+ros::Subscriber sub = nh.subscribe("/camera/image_raw", 1, dlib_callback);
+ros::spin();
+}
---
+++ src/vision/facial_puppeteery/src/pupeteery_core.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+import sys
+import rospy
+
+import roslib
+import cv2
+
+from pau2motors.msg import pau
+from facial_puppetry.msg import land_marks
+from cv_bridge import CvBridge, CvBridgeError
+roslib.load_manifest('facial_puppetry')
+
+
+'''
+This Class performs Dlib to Blender Shapekey Mapping. The current capability of DLIB does not let us address
+all shapekeys in Sophia.blend. For instance, mapping high resolution shapekeys like brow_inner_UP.L, brow_inner_UP.R ..
+may not yield better result. Hence, those requirements are underlined as features to be added in this package.
+
+ '''
+
+
+class facial_puppetry:
+
+  # declare publishers, subscribers, and other important static variables
+  def __init__(self):
+    self.pub_pau = rospy.Publisher('/blender_api/set_pau', pau, queue_size=10)
+    self.baseline = []
+    self.bridge = CvBridge()
+    self.image_sub = rospy.Subscriber("/dlib_values", land_marks, self.get_dlib_val)
+    self.width = 0
+    self.hight = 0
+    self.count = 0
+    self.shapekey_name = ['brow_center_UP', 'brow_center_DN', 'brow_inner_UP.L', 'brow_inner_DN.L', 'brow_inner_UP.R',
+                          'brow_inner_DN.R', 'brow_outer_UP.L', 'brow_outer_DN.L', 'brow_outer_up.R', 'brow_outer_DN.R',
+                          'eye-flare.UP.L', 'eye-blink.UP.L', 'eye-flare.UP.R', 'eye-blink.UP.R', 'eye-blink.LO.L',
+                          'eye-flare.LO.L', 'eye-blink.LO.R', 'eye-flare.LO.R', 'wince.L', 'wince.R', 'sneer.L',
+                          'sneer.R', 'eyes-look.dn', 'eyes-look.up', 'lip-UP.C.UP', 'lip-UP.C.DN', 'lip-UP.L.UP',
+                          'lip-UP.L.DN', 'lip-UP.R.UP', 'lip-UP.R.DN', 'lips-smile.L','lips-smile.R', 'lips-wide.L',
+                          'lips-narrow.L', 'lips-wide.R', 'lips-narrow.R', 'lip-DN.C.DN', 'lip-DN.C.UP', 'lip-DN.L.DN',
+                          'lip-DN.L.UP', 'lip-DN.R.DN', 'lip-DN.R.UP', 'lips-frown.L', 'lips-frown.R','lip-JAW.DN']
+    self.max = []
+  
+
+  # The final DLIB LM - Blender Shapekey mapping is here
+  def map_to_Sophia(self, baseline_dist, current_dist, maxN):
+      final_val = []
+      #put handled shapekey indexs here
+      H_shapekeys = [0,2,4,6,8,10,11,12,13,14,15,16,17,20,21,30,31,32,34,44]
+
+      for i in range(0,45):
+          MAX = 1; MIN = 0
+          if i in H_shapekeys:
+              if i == 44:
+                  MAX = 0.5
+              # if i == 0 or  i== 2 or i==4 or i==6 or  i==8 or  i == 10 or i == 11 or i == 12 or i == 13 or i ==14 or i == 15 or i == 16 or i == 17 \
+              #         or i == 20 or  i == 21 or  i == 30 or i == 31 or  i == 32 or i == 34 :
+              blend_val= min(MAX,max(MIN, (current_dist[i] - baseline_dist[i])/maxN[i])) #make sure the values are in b/n 0 and 1
+              final_val.append(blend_val)
+
+              # elif i ==44:
+              #     blend_val = min(0.6, max(0, (current_dist[i] - baseline_dist[i]) / maxN[i]))
+              #     final_val.append(blend_val)
+          else: #if it is not handled put 0
+              final_val.append(0.0)
+      return final_val
+
+  # callback: gets relative distance of DLIB's land marks per their corresponding shapekey
+  def get_dlib_val(self, data):
+      # sampling point: change this with event driven value
+      if self.count == 5:
+          self.baseline = data.dlib_val
+          self.max = data.max_ref
+
+          #not used for now
+          self.width = data.distX
+          self.hight = data.distY
+
+      #change this with event_driven condition -- UI based
+      if self.count >10:
+          #Head Position and Orientation manipulation
+          head_pau = pau()
+
+          head_pau.m_headRotation.x = 0.9
+          head_pau.m_headRotation.y = 0.5
+          head_pau.m_headRotation.z = 0.7
+          head_pau.m_headRotation.w = 0.9
+
+          head_pau.m_headTranslation.x =0.9
+          head_pau.m_headTranslation.y =0.7
+          head_pau.m_headTranslation.z =0.8
+
+
+          head_pau.m_neckRotation.x= -0.9
+          head_pau.m_neckRotation.y= -0.5
+          head_pau.m_neckRotation.z= 0.5
+          head_pau.m_neckRotation.w= 0.9
+
+
+          #Gaze Position and Orientation manipulation
+          head_pau.m_eyeGazeLeftPitch = 0.01
+          head_pau.m_eyeGazeLeftYaw = 0.1
+          head_pau.m_eyeGazeRightPitch = 0.01
+          head_pau.m_eyeGazeRightYaw = 0.1
+
+
+          # assign final values to pau
+          head_pau.m_coeffs = self.map_to_Sophia(self.baseline, data.dlib_val, self.max)
+          head_pau.m_shapekeys = self.shapekey_name
+
+          #publish to pau
+          self.pub_pau.publish(head_pau)
+      self.count += 1
+
+def main(args):
+    rospy.init_node('puppeteer_core', anonymous=True)
+    facial_puppetry()
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Facial Puppetry- Blendshape Mapper Shutting Down")
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main(sys.argv)
