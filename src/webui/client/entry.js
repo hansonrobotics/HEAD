63d965b1c1cd8fc72024b5d4bb238471d426a0ca
Henrikas Elsbergas <elsbergass@gmail.com>
2016-09-27 13:24:57 +0300

    Fix mobile error

--- src/webui/client/entry.js
+++ src/webui/client/entry.js
@@ -1,4 +1,4 @@
-require(['eventemitter2', 'application', 'jquery-ui-touch-punch', 'bootstrap', 'application', 'modules/puppeteering/router',
+require(['eventemitter2', 'application', 'jquery-ui', 'jquery-ui-touch-punch', 'bootstrap', 'application', 'modules/puppeteering/router',
         'modules/animations/animations_app',
         'modules/expressions/expressions_app',
         'modules/motors/motors_app',
