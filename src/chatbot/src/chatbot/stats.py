1c645e7f0a1533c64169483628d6e0152df0547f
Wenwei Huang <dut.hww@gmail.com>
2016-09-27 18:07:39 +0800

    Get pattern frequency

--- src/chatbot/src/chatbot/stats.py
+++ src/chatbot/src/chatbot/stats.py
@@ -2,12 +2,17 @@ import os
 import logging
 import pandas as pd
 import glob
+import re
 import datetime as dt
+from client import Client
+from collections import Counter
 
 logger = logging.getLogger('hr.chatbot.stats')
+trace_pattern = re.compile(
+    r'../(?P<fname>.*), (?P<tloc>\(.*\)), (?P<pname>.*), (?P<ploc>\(.*\))')
 
 
-def history_stats(history_dir, days):
+def collect_history_data(history_dir, days):
     today = dt.datetime.now()
     dfs = []
     for d in glob.glob('{}/*'.format(history_dir)):
@@ -25,12 +30,17 @@ def history_stats(history_dir, days):
                     except Exception as ex:
                         logger.warn("Reading {} error: {}".format(fname, ex))
     if not dfs:
-        return {}
+        return None
     df = pd.concat(dfs, ignore_index=True)
     df = df[df.Datetime != 'Datetime'].sort(
         ['User', 'Datetime']).drop_duplicates()
-    if hasattr(df, 'Lang'):
-        df = df[df.Lang.isin(['en'])]
+    return df
+
+
+def history_stats(history_dir, days):
+    df = collect_history_data(history_dir, days)
+    if df is None:
+        return {}
     if days == -1:
         stats_csv = '{}/full_history.csv'.format(history_dir)
     else:
@@ -54,8 +64,39 @@ def history_stats(history_dir, days):
     }
     return response
 
+
+def playback_history(df):
+    client = Client(os.environ.get('HR_CHATBOT_AUTHKEY', 'AAAAB3NzaC'), test=True)
+    pattern_column = []
+    for question in df.Question:
+        answer = client.ask(question, True)
+        traces = answer.get('trace')
+        patterns = []
+        if traces:
+            for trace in traces:
+                match_obj = trace_pattern.match(trace)
+                if match_obj:
+                    patterns.append(match_obj.group('pname'))
+        pattern_column.append(patterns)
+    df.loc[:,'Pattern'] = pd.Series(pattern_column, index=df.index)
+    return df
+
+def pattern_stats(history_dir, days):
+    df = collect_history_data(history_dir, days)
+    if df is None:
+        return {}
+    df = playback_history(df)
+    patterns = sum(df.Pattern, [])
+    counter = Counter(patterns)
+    pattern_freq = pd.Series(counter)
+    pattern_freq.sort(ascending=False)
+    stats_csv = '{}/pattern_frequency.csv'.format(history_dir)
+    pattern_freq.to_csv(stats_csv)
+    logger.info("Write pattern statistic to {}".format(stats_csv))
+
 if __name__ == '__main__':
     logging.basicConfig()
     logging.getLogger().setLevel(logging.INFO)
     history_stats(os.path.expanduser('~/.hr/chatbot/history'), -1)
     history_stats(os.path.expanduser('~/.hr/chatbot/history'), 7)
+    pattern_stats(os.path.expanduser('~/.hr/chatbot/history'), -1)
