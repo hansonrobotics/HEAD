956ea660fd8a1727a5350f0da0a092a1e9466066
Wenwei Huang <dut.hww@gmail.com>
2016-11-01 14:11:47 +0800

    Add location and weather api

--- src/chatbot/src/chatbot/utils.py
+++ src/chatbot/src/chatbot/utils.py
@@ -1,4 +1,8 @@
 import re
+import os
+import requests
+import subprocess
+
 
 def norm(s):
     if s is None:
@@ -18,6 +22,17 @@ def shorten(text, cutoff):
             break
         ret += (sen+'.')
     return ret, '.'.join(sens[idx:])
+
+def get_location():
+    # docker run --net=host --restart=always -d fiorix/freegeoip
+    ip = subprocess.check_output(['dig', '+short', 'myip.opendns.com', '@resolver1.opendns.com']).strip()
+    location = requests.get('http://localhost:8080/json/{ip}'.format(ip=ip)).json()
+    return location
+
+def get_weather(city):
+    OPENWEATHERAPPID = os.environ.get('OPENWEATHERAPPID')
+    response = requests.get('http://api.openweathermap.org/data/2.5/weather', params={'q': city, 'appid': OPENWEATHERAPPID}).json()
+    return response
 
 if __name__ == '__main__':
     text = '''My mind is built using Hanson Robotics' character engine, a simulated humanlike brain that runs inside a personal computer. Within this framework, Hanson has modelled Phil's personality and emotions, allowing you to talk with Phil through me, using speech recognition, natural language understanding, and computer vision such as face recognition, and animation of the robotic muscles in my face.'''
