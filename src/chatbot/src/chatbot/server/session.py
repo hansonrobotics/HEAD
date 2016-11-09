caf4a5b8262efa08dd5603705d25b8951897472a
Wenwei Huang <dut.hww@gmail.com>
2016-11-09 18:49:07 +0800

    Update class SessionData

--- src/chatbot/src/chatbot/server/session.py
+++ src/chatbot/src/chatbot/server/session.py
@@ -13,8 +13,18 @@ logger = logging.getLogger('hr.chatbot.server.session')
 
 
 class SessionData(object):
-    pass
 
+    def __init__(self):
+        self.context = defaultdict(dict)
+
+    def set_context(self, cid, context):
+        self.context[cid].update(context)
+
+    def get_context(self, cid):
+        return self.context[cid]
+
+    def reset_context(self, cid):
+        self.context[cid] = {}
 
 class Session(object):
 
