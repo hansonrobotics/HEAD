6d64d77dfac891ac577f7c9e6266917c789d936f
Wenwei Huang <dut.hww@gmail.com>
2017-01-23 17:19:38 +0800

    Set CS host and port specified in env

--- src/chatbot/src/chatbot/server/config.py
+++ src/chatbot/src/chatbot/server/config.py
@@ -13,6 +13,8 @@ SESSION_REMOVE_TIMEOUT = 600  # Timeout seconds for a session to be removed
 CHATBOT_LOG_DIR = os.environ.get('CHATBOT_LOG_DIR') or os.path.expanduser('~/.hr/chatbot')
 HISTORY_DIR = os.path.join(CHATBOT_LOG_DIR, 'history')
 TEST_HISTORY_DIR = os.path.join(CHATBOT_LOG_DIR, 'test/history')
+CS_HOST = os.environ.get('CS_HOST') or 'localhost'
+CS_PORT = os.environ.get('CS_PORT') or '1024'
 
 SOLR_URL = 'http://localhost:8983'
 
