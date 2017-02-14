8cc1db30ae0751e7d0e5ad2dd8344047a84262a3
Wenwei Huang <dut.hww@gmail.com>
2017-02-14 14:33:09 +0800

    Work with multiple servies

--- src/vision/scripts/face_tracker.py
+++ src/vision/scripts/face_tracker.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from threading import Thread
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
 import rospy
@@ -12,22 +13,32 @@ sys.path.insert(0, os.path.join(CWD, '../src'))
 
 from vision.client import Client
 
+hosts = ['http://localhost:9001', 'http://localhost:9002', 'http://localhost:9003', 'http://localhost:9004']
 bridge = CvBridge()
-client = Client()
+client = Client(hosts)
+image_cache = {}
+processed_images = {}
 
 rospy.init_node('face_tracker')
 imgpub = rospy.Publisher('~image', Image, latch=True, queue_size=1)
 
-def cb(ros_image):
-    image = bridge.imgmsg_to_cv2(ros_image, "bgr8")
-    retval, buf = cv2.imencode('.png', image)
-    f = BytesIO(buf)
-    response = client.detect_faces(f)
+def callback(frame, response):
+    processed_images[frame] = response
+    image = image_cache[frame]
     faces = response['faces']
-
     for left, top, right, bottom in faces:
         cv2.rectangle(image, (left, top), (right, bottom), (255, 0, 0), 2)
     imgpub.publish(bridge.cv2_to_imgmsg(image, 'bgr8'))
+    del image_cache[frame]
+    del processed_images[frame]
+
+def cb(ros_image):
+    frame = ros_image.header.seq
+    image = bridge.imgmsg_to_cv2(ros_image, "bgr8")
+    retval, buf = cv2.imencode('.png', image)
+    f = BytesIO(buf)
+    client.detect_faces(frame, f, callback)
+    image_cache[frame] = image
 
 rospy.Subscriber('/camera/image_raw', Image, cb)
 
--- src/vision/src/vision/client.py
+++ src/vision/src/vision/client.py
@@ -1,41 +1,77 @@
 import requests
 import os
 import datetime as dt
+import subprocess
+from Queue import Queue
+from threading import Thread
+from io import BytesIO
+from itertools import repeat
 
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-DEFAULT_HOST = 'http://localhost'
-DEFAULT_PORT = '9001'
+DEFAULT_HOST_URL = 'http://localhost:9001'
 
 class Client(object):
 
     VERSION = 'v1.0'
 
-    def __init__(self, host=DEFAULT_HOST, port=DEFAULT_PORT):
-        self.host = host
-        self.port = port
-        self.root_url = '{}:{}/{}'.format(self.host, self.port, Client.VERSION)
+    class Worker(Thread):
 
-    def detect_faces(self, image_file):
+        def __init__(self, incoming_queue, results):
+            super(Client.Worker, self).__init__()
+            self.incoming_queue = incoming_queue
+            self.results = results
+
+        def run(self):
+            while True:
+                frame, image_file, url, callback = self.incoming_queue.get()
+                try:
+                    r = requests.post(
+                        '{}/detect_face'.format(url), data=image_file, timeout=1)
+                    ret = r.json().get('ret')
+                    response = r.json().get('response')
+                    if callable(callback):
+                        callback(frame, response)
+                    self.results.put((frame, response))
+                    self.incoming_queue.task_done()
+                except Exception as ex:
+                    print ex
+
+    def __init__(self, hosts=None):
+        if not hosts:
+            self.hosts = [DEFAULT_HOST_URL]
+        else:
+            self.hosts = hosts
+        self.q = Queue()
+        self.results = Queue()
+
+        self.workers = {}
+        for host in self.hosts:
+            worker = Client.Worker(self.q, self.results)
+            worker.daemon = True
+            worker.start()
+            self.workers[host] = worker
+
+    def detect_faces(self, frame, image_file, callback=None):
         """
         image_file: file-like object
         """
-        try:
-            r = requests.post(
-                '{}/detect_face'.format(self.root_url), data=image_file)
-            ret = r.json().get('ret')
-            response = r.json().get('response')
-            return response
-        except Exception as ex:
-            print ex
-
+        host = self.hosts[frame%len(self.hosts)]
+        url = '{}/{}'.format(host, Client.VERSION)
+        buf = BytesIO(image_file.read())
+        self.q.put((frame, buf, url, callback))
 
 if __name__ == '__main__':
-    client = Client()
+    hosts = ['http://localhost:9001', 'http://localhost:9002']
+    client = Client(hosts)
     start = dt.datetime.now()
+    def callback(frame, response):
+        print frame, response
+
     for i in range(100):
         with open('obama.png', 'rb') as f:
-            print client.detect_faces(f)
-            print dt.datetime.now() - start
+            client.detect_faces(i, f, callback)
+    client.q.join()
+    print dt.datetime.now() - start
