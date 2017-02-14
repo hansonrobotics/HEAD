2ec2751cbcd453e2da560da770aaceba2300d2d2
Wenwei Huang <dut.hww@gmail.com>
2017-02-14 16:02:24 +0800

    Set dlib face detection as default

--- src/vision/src/vision/face_detector.py
+++ src/vision/src/vision/face_detector.py
@@ -2,11 +2,13 @@ import dlib
 import numpy as np
 import cv2
 
-face_detector = dlib.get_frontal_face_detector()
+dlib_face_detector = dlib.get_frontal_face_detector()
 
-def detect_face(image, max_results=4):
+def dlib_detect_face(image, max_results=4):
     nparr = np.fromstring(image, np.uint8)
     image = cv2.imdecode(nparr, 1)
     if image is not None and image.any():
-        detected_faces = face_detector(image)
+        detected_faces = dlib_face_detector(image)
         return detected_faces
+
+detect_face = dlib_detect_face
