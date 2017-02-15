0054175662f569f1fdbfe157fb6ba3bda69d90a6
Wenwei Huang <dut.hww@gmail.com>
2017-02-15 14:04:52 +0800

    Stop immediately if chatbot disabled

--- src/chatbot/scripts/ai.py
+++ src/chatbot/scripts/ai.py
@@ -134,6 +134,7 @@ class Chatbot():
 
     def _request_callback(self, chat_message):
         if not self.enable:
+            self.client.cancel_timer()
             logger.info("Chatbot is disabled")
             return
         if 'shut up' in chat_message.utterance.lower():
