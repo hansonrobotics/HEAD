1252b2daf9166a8f201c7947a1ebbdf56c188746
Wenwei Huang <dut.hww@gmail.com>
2017-02-16 12:02:40 +0800

    Print FPS

--- src/vision/scripts/face_tracker.py
+++ src/vision/scripts/face_tracker.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from __future__ import division
 from threading import Thread
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
@@ -8,6 +9,7 @@ from io import BytesIO
 import cv2
 import os
 import sys
+import time
 CWD = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(0, os.path.join(CWD, '../src'))
 
@@ -18,16 +20,30 @@ bridge = CvBridge()
 client = Client(hosts)
 image_cache = {}
 processed_images = {}
+start = time.time()
+counter = 0
+fps = 0
+interval = 2
 
 rospy.init_node('face_tracker')
 imgpub = rospy.Publisher('~image', Image, latch=True, queue_size=1)
 
+
 def callback(frame, response):
+    global fps, counter, start
+    now = time.time()
+    counter += 1
+    if (now-start) > interval:
+        fps = counter/interval
+        start = now
+        counter = 0
     processed_images[frame] = response
     image = image_cache[frame]
     faces = response['faces']
+
+    cv2.putText(image, 'FPS: {}'.format(fps), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 1)
     for left, top, right, bottom in faces:
-        cv2.rectangle(image, (left, top), (right, bottom), (255, 0, 0), 2)
+        cv2.rectangle(image, (left, top), (right, bottom), (255, 0, 0), 1)
 
     all_landmarks = response.get('landmarks')
     if all_landmarks:
