98a6e137e6b3dd46221de50bd6f9298895a010d5
Wenwei Huang <dut.hww@gmail.com>
2017-03-20 13:54:34 +0800

    Read workspace

--- scripts/hr-completion.bash
+++ scripts/hr-completion.bash
@@ -9,7 +9,7 @@
 
 _hr() {
   local command
-  local hr_commands="init repo install build clean run"
+  local hr_commands="init get install build clean run env"
   local cur=${COMP_WORDS[COMP_CWORD]}
   local words=${COMP_WORDS[@]}
   local cword=$COMP_CWORD
