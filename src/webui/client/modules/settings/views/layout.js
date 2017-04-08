ef9736e37c5f2c567dfb65c970a0a53e3b549254
Henrikas Elsbergas <elsbergass@gmail.com>
2017-04-08 16:05:30 +0300

    Making admin settings layout fluid, hiding robot settings

--- src/webui/client/modules/settings/views/layout.js
+++ src/webui/client/modules/settings/views/layout.js
@@ -10,40 +10,64 @@ define(['marionette', './templates/layout.tpl', 'lib/regions/fade_in', 'jquery',
                 }
             },
             ui: {
+                settingsContainer: '.app-settings-container',
                 navigation: '.app-settings-navigation',
+                nav: '.app-settings-navigation > nav',
                 nodeList: '.app-nav-node-list',
                 navLinks: '.app-settings-navigation a'
             },
             events: {
-                'click @ui.navLinks': 'collapseNav'
+                'click @ui.navLinks': 'navLinkClick'
+            },
+            config: {
+                mobileWidth: 800
+            },
+            initialize: function() {
+                // load first node
+                this.init = true
             },
             onAttach: function() {
-            },
-            onDomRefresh: function() {
-                let self = this,
-                    updateNavHeight = function() {
-                        if (self.isDestroyed())
-                            $(window).off("resize", updateNavHeight)
-                        else {
-                            self.ui.navigation.multilevelpushmenu('option', 'menuHeight', $(window).height())
-                            self.ui.navigation.multilevelpushmenu('redraw')
-                            $('.levelHolderClass', self.ui.navigation).perfectScrollbar('update')
-                        }
-                    }
-
+                this.mobile = $(window).width() < this.config.mobileWidth;
                 this.ui.navigation.multilevelpushmenu({
                     container: this.ui.navigation,
                     menuWidth: '250px',
                     menuHeight: $(window).height(),
-                    collapsed: true,
-                    preventItemClick: false
+                    collapsed: this.mobile,
+                    swipe: false
                 })
 
-                $('.levelHolderClass', this.ui.navigation).perfectScrollbar()
+                _.bindAll(this, ['resizeCallback'])
+                this.resizeCallback()
 
-                $(window).on("resize", updateNavHeight)
+                $(this.ui.nav).perfectScrollbar()
+                $(window).on("resize", this.resizeCallback)
+
                 this.nodeListInterval = setInterval(_.bind(this.updateNodeList, this), 10000)
                 this.updateNodeList()
+            },
+            resizeCallback: function() {
+                this.ui.navigation.multilevelpushmenu('option', 'menuHeight', $(window).height())
+                this.ui.navigation.multilevelpushmenu('redraw')
+                $(this.ui.nav).perfectScrollbar('update')
+
+                this.mobile = $(window).width() < this.config.mobileWidth;
+
+                if (this.mobile) {
+                    this.ui.navigation.find('.cursorPointer').fadeIn()
+                    this.ui.settingsContainer.addClass('mobile')
+                } else {
+                    this.ui.navigation.find('.cursorPointer').hide()
+                    this.ui.settingsContainer.removeClass('mobile')
+                    this.expandNav()
+                }
+            },
+            navLinkClick: function(e) {
+                if (this.mobile)
+                    this.collapseNav()
+                $(e.target).blur()
+            },
+            expandNav: function(e) {
+                $(this.ui.navigation).multilevelpushmenu('expand')
             },
             collapseNav: function(e) {
                 $(this.ui.navigation).multilevelpushmenu('collapse')
@@ -52,14 +76,22 @@ define(['marionette', './templates/layout.tpl', 'lib/regions/fade_in', 'jquery',
                 let self = this
                 api.services.get_configurable_nodes.callService({}, function(response) {
                     let container = $('<div>')
+
                     $.each(response.nodes, function() {
+                        let url = '/admin/settings/node/' + encodeURIComponent(this)
+
+                        // go to first node settings on initialization
+                        if (self.init) {
+                            self.init = false
+                            Backbone.history.navigate(url, {trigger: true})
+                        }
                         let link = $('<a>').prop({
-                            href: '#/admin/settings/node/' + encodeURIComponent(this)
+                            href: '#' + url
                         }).html(this)
                         container.append($('<li>').append(link))
                     })
                     self.ui.nodeList.html(container.html())
-                    $('.levelHolderClass', self.ui.navigation).perfectScrollbar('update')
+                    $(self.ui.nav).perfectScrollbar('update')
                 }, function(error) {
                     console.log(error)
                 })
