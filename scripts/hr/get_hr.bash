50bfc7d853619230af17f5d075098e3e0c8ed740
Wenwei Huang <dut.hww@gmail.com>
2017-04-13 17:13:59 +0800

    Update hr

--- scripts/hr/get_hr.bash
+++ scripts/hr/get_hr.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -e
-version=0.1.8
+version=0.1.9
 curl -sLo /tmp/head-hr_${version}_amd64.deb https://github.com/hansonrobotics/hrtool/releases/download/v${version}/head-hr_${version}_amd64.deb
 if [[ -f /usr/local/bin/hr-base ]]; then
     sudo rm /usr/local/bin/hr
@@ -9,3 +9,4 @@ if [[ -f /usr/local/bin/hr-base ]]; then
     sudo rm /usr/local/bin/hr-ext
 fi
 sudo dpkg -i /tmp/head-hr_${version}_amd64.deb
+rm /tmp/head-hr_${version}_amd64.deb
