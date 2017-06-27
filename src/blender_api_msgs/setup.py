862564cad7139ab6db41b7187ba7c21957d6b501
Wenwei Huang <dut.hww@gmail.com>
2017-06-27 16:36:01 +0800

    Bump blender_api_msgs version

--- src/blender_api_msgs/setup.py
+++ src/blender_api_msgs/setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "roscom-blender-api",
-    version = "0.2",
+    version = "0.3.0",
     package_dir = {'': 'src',},
     packages = find_packages('src'),
     entry_points = {
