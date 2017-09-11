6a5e48a1be93ee1da07aff73c980a2b2018934ec
Wenwei Huang <dut.hww@gmail.com>
2017-09-11 19:14:39 +0800

    Fix launch issues

--- scripts/main.sh
+++ scripts/main.sh
@@ -7,26 +7,15 @@
 
 set -e
 
-COLOR_INFO='\033[32m'
-COLOR_WARN='\033[33m'
-COLOR_ERROR='\033[31m'
-COLOR_RESET='\033[0m'
-info() {
-    printf "${COLOR_INFO}[INFO] ${1}${COLOR_RESET}\n"
-}
-warn() {
-    printf "${COLOR_WARN}[WARN] ${1}${COLOR_RESET}\n"
-}
-error() {
-    printf "${COLOR_ERROR}[ERROR] ${1}${COLOR_RESET}\n"
-}
+export BASEDIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+source $BASEDIR/common.sh
 
 show_help() {
 cat << EOF
 Usage: $0 OPTIONS <robot name>
 
 Options are:
-  --puppeteering|-p     Launch puppeteering
+  --puppeteering|-p <IP address>    Launch puppeteering. IP address is the one of the machine that runs Faceshift
   --dev|-d              Run in dev mode
   --layout              Rearrange the applications layout
   --tracker tracker     Specify which tracker: pi_vision(default), cmt, or realsense
@@ -34,20 +23,37 @@ Options are:
   --body body           Specify body name (for dynamixel)
   --autoname            Automatically discover robot name by hardware
   --autobody            Automatically discover robot body by hardware
+  --dynamixel <device path>         Dynamixel device name
   --help|-h             Show this help
 
 EOF
 }
 
+# cs 1024
+# https 4000
+# http 8000
+# chatbot 8001
+# ws 9090
+# ttsserver 10001
+check_ports 1024 4000 8000 8001 9090 10001
+check_disk_usage
 export CMT_TRACKING=0
 export PI_VISION_TRACKING=0
 export REALSENSE_TRACKING=0
-export BASEDIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 
 while [[ $# > 0 ]]; do
     case "$1" in
         --puppeteering|-p)
             export PUPPETEERING=1
+            ip=$2
+            [[ -z $ip ]] && error "-p requires IP address" && exit 1
+            if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+                export PUPPETEERING_IP=$ip
+            else
+                error "Invalid IP address \"$ip\""
+                exit 1
+            fi
+            shift
             shift
             ;;
         --dev|-d)
@@ -88,6 +94,17 @@ while [[ $# > 0 ]]; do
             export NO_GUI=1
             shift
             ;;
+        --dynamixel)
+            if [[ -z $2 ]]; then
+                error "--dynamixel option is incorrect" >&2
+                show_help
+                exit 1
+            fi
+            export DYNAMIXEL=1
+            export DYNAMIXEL_DEVICE=$2
+            shift
+            shift
+            ;;
         --body)
             export BODY=$2
             shift
@@ -117,11 +134,48 @@ while [[ $# > 0 ]]; do
     esac
 done
 
+if [[ ! "$(id -nG)" =~ .*"dialout".* ]]; then
+    warn "You are not in the \"dialout\" group"
+fi
+
+source ~/.hr/env.sh
+source $HR_PREFIX/ros/setup.bash
+export -f _ros_decode_path _ros_list_locations _ros_list_packages _ros_list_stacks _ros_location_find _ros_package_find _roscmd _roscomplete _roscomplete_exe _roscomplete_file _roscomplete_find _roscomplete_launch _roscomplete_node_transform _roscomplete_pkg _roscomplete_rosbag _roscomplete_rosclean _roscomplete_rosconsole _roscomplete_roscreate_pkg _roscomplete_rosmake _roscomplete_rosmsg _roscomplete_rosnode _roscomplete_rospack _roscomplete_rosparam _roscomplete_rosrun_transform _roscomplete_rosservice _roscomplete_rossrv _roscomplete_rostopic _roscomplete_roswtf _roscomplete_search_dir _roscomplete_sub_dir _roscomplete_test _rosfind _rossed rosawesome roscat roscd roscp rosd rosed rosls rospd rospython
+
+if [[ $HR_ROLE == 'developer' ]]; then
+    if [[ -z $HR_WORKSPACE ]]; then
+        error "HR_WORKSPACE is not found"
+        exit 1
+    else
+        info HR_WORKSPACE=$HR_WORKSPACE
+    fi
+fi
+
+if [[ -z $HR_PREFIX ]]; then
+    error "HR_PREFIX is not found"
+    exit 1
+fi
+
+export ROBOTS_CONFIG_DIR=$BASEDIR/robots_config
+export ROS_PYTHON_LOG_CONFIG_FILE="$BASEDIR/python_logging.conf"
+export ROSCONSOLE_FORMAT='[${logger}][${severity}] [${time}]: ${message}'
+export LOCATION_SERVER_HOST=52.41.5.107
+export LOCATION_SERVER_PORT=8004
+export OPENWEATHERAPPID='1fe4806fc62327aa719a2bb215e983fe'
+export CITY_LIST_FILE=${HR_PREFIX}/data/city.list.json
+export MARKY_MARKOV_DIR='/opt/hansonrobotics/tools/marky_markov'
+export PKD_MODEL="$MARKY_MARKOV_DIR/models/pkd"
+export DISPLAY=:0
+export PY2ENV_DIR=/opt/hansonrobotics/py2env
+export PY3ENV_DIR=/opt/hansonrobotics/py3env
+export PY2PATH=$PY2ENV_DIR/lib/python2.7/site-packages:$PY2ENV_DIR/lib/python2.7/dist-packages:/usr/lib/python2.7/dist-packages
+export PY3PATH=$PY3ENV_DIR/lib/python3.4/site-packages:$PY3ENV_DIR/lib/python3.4/dist-packages
+
 if [[ -z $NAME ]]; then
     if [[ $AUTONAME == 1 ]]; then
         info "Discovering robot name"
         while [[ -z $NAME ]]; do
-            NAME=$($BASEDIR/discover.py --dirname $BASEDIR/robots_config |grep "robot name"|cut -d: -f2)
+            NAME=$($BASEDIR/discover.py --dirname $ROBOTS_CONFIG_DIR|grep "robot name"|cut -d: -f2)
             if [[ -z $NAME ]]; then
                 error "Can't discover robot name"
             fi
@@ -134,6 +188,9 @@ if [[ -z $NAME ]]; then
         exit 1
     fi
 fi
+
+export HR_CHARACTER_PATH=$HR_WORKSPACE/HEAD/src/chatbot/scripts/characters
+export HR_CHATBOT_REVISION=$(git -C $HR_WORKSPACE/HEAD log -1 --format='%h' 2>/dev/null)
 
 if [[ -z $BODY ]]; then
     if [[ $AUTOBODY == 1 ]]; then
@@ -166,33 +223,6 @@ case $NAME in
         exit 1
 esac
 
-if [[ ! "$(id -nG)" =~ .*"dialout".* ]]; then
-    warn "You are not in the \"dialout\" group"
-fi
-
-source ~/.hr/env.sh
-
-if [[ -z $HR_WORKSPACE ]]; then
-    error "HR_WORKSPACE is not found"
-    exit 1
-else
-    info HR_WORKSPACE=$HR_WORKSPACE
-fi
-
-if [[ -z $HR_PREFIX ]]; then
-    error "HR_PREFIX is not found"
-    exit 1
-fi
-
-export ROS_PYTHON_LOG_CONFIG_FILE="$BASEDIR/python_logging.conf"
-export ROSCONSOLE_FORMAT='[${logger}][${severity}] [${time}]: ${message}'
-export ROBOTS_CONFIG_DIR=$BASEDIR/robots_config
-export HR_CHARACTER_PATH=$HR_WORKSPACE/HEAD/src/chatbot/scripts/characters
-export HR_CHATBOT_REVISION=$(git -C $HR_WORKSPACE/HEAD log -1 --format='%h' 2>/dev/null)
-export LOCATION_SERVER_HOST=52.41.5.107
-export LOCATION_SERVER_PORT=8004
-export OPENWEATHERAPPID='1fe4806fc62327aa719a2bb215e983fe'
-
 if [[ $(tmux ls 2>/dev/null) == ${NAME}* ]]; then
     tmux kill-session -t $NAME
     info "Killed session $NAME"
@@ -204,15 +234,21 @@ if [[ $DEV_MODE == 1 ]]; then
     ros_args="$ros_args dev:=true"
 fi
 
-source /opt/hansonrobotics/ros/setup.bash
-source $HR_WORKSPACE/HEAD/devel/setup.bash
+if [[ $HR_ROLE == 'developer' ]]; then
+    if [[ -f $HR_WORKSPACE/HEAD/devel/setup.bash ]]; then
+        source $HR_WORKSPACE/HEAD/devel/setup.bash
+    else
+        error "Workspace is not built. Run 'hr build head'"
+        exit 1
+    fi
+fi
 
 if [[ $NO_GUI == 1 ]]; then
     ros_args="$ros_args gui:=false"
 fi
 
 if [[ $PUPPETEERING == 1 ]]; then
-    ros_args="$ros_args puppeteering:=true"
+    ros_args="$ros_args puppeteering:=true puppeteering_ip:=$PUPPETEERING_IP"
 fi
 
 if [[ $PI_VISION_TRACKING == 0 && $REALSENSE_TRACKING == 0 && $CMT_TRACKING == 0 ]]; then
@@ -231,28 +267,46 @@ if [[ $CMT_TRACKING == 1 ]]; then
     ros_args="$ros_args pi_vision:=false realsense:=false cmt:=true"
 fi
 
+if [[ $DYNAMIXEL == 1 ]]; then
+   if [[ ! -e $DYNAMIXEL_DEVICE ]]; then
+        error "Dynamixel device \"$DYNAMIXEL_DEVICE\" is not found"
+        exit 1
+   fi
+   ros_args="$ros_args dynamixel_usb:=true dynamixel_device:=$DYNAMIXEL_DEVICE"
+fi
+
 if [[ ! -z $BODY ]]; then
+    if [[ $DYNAMIXEL == 1 ]]; then
+        error "Can't both specify --dynamixel with --body or --autobody"
+        exit 1
+    fi
     dynamixel=/dev/hr/body/$BODY/dynamixel
     if [[ -e $dynamixel ]]; then
         ros_args="$ros_args dynamixel_usb:=true dynamixel_device:=${dynamixel}"
     else
-        error "\"${dynamixel}\" doesn't exist"
+        error "Dynamixel device \"$dynamixel\" is not found"
         exit 1
     fi
 fi
 
 while rostopic list >/dev/null 2>&1; do error "roscore is running. Waiting." >&2; sleep 1; done
 
-warn "ROS launch args \"${ros_args}\""
+warn "ROS launch args:"
+for arg in ${ros_args}; do warn $arg; done
 
-tmux new-session -d -s $NAME -n $NAME "roslaunch ${ROBOTS_CONFIG_DIR}/robot.launch $ros_args; $SHELL"
+tmux new-session -d -s $NAME -n $NAME "export PYTHONPATH=$PY2PATH:$PYTHONPATH && roslaunch ${ROBOTS_CONFIG_DIR}/robot.launch $ros_args; $SHELL"
 until rostopic list >/dev/null 2>&1; do sleep 1; info "Waiting for master"; done
 
 info "Starting"
 
-tmux new-window -n 'rosbridge_ssl' "roslaunch ${ROBOTS_CONFIG_DIR}/rosbridge_ssl.launch ssl:=true certfile:=$HR_WORKSPACE/HEAD/src/webui/backend/ssl/cert.crt keyfile:=$HR_WORKSPACE/HEAD/src/webui/backend/ssl/key.pem port:=9094"
+if [[ $HR_ROLE == 'developer' ]]; then
+    WEBUI_HOME=$HR_WORKSPACE/HEAD/src/webui
+else
+    WEBUI_HOME=/opt/hansonrobotics/ros/share/webui
+fi
 
-WEBUI_SERVER=$HR_WORKSPACE/HEAD/src/webui/backend/entry.js
+tmux new-window -n 'rosbridge_ssl' "roslaunch ${ROBOTS_CONFIG_DIR}/rosbridge_ssl.launch ssl:=true certfile:=$WEBUI_HOME/backend/ssl/cert.crt keyfile:=$WEBUI_HOME/backend/ssl/key.pem port:=9094"
+
 WEBPACK_OPTIONS="--optimize-minimize"
 NODE_INTERPRETER="node"
 
@@ -261,20 +315,25 @@ if [[ $DEV_MODE == 1 ]]; then
   NODE_INTERPRETER="nodemon"
 fi
 
-tmux new-window -n 'webui' "cd $HR_WORKSPACE/HEAD/src/webui; webpack $WEBPACK_OPTIONS &
+WEBUI_SERVER=$WEBUI_HOME/backend/entry.js
+if [[ ! -f $WEBUI_SERVER ]]; then
+    error "webui is not installed. Run 'hr install head-webui'"
+fi
+
+tmux new-window -n 'webui' "
     $NODE_INTERPRETER $WEBUI_SERVER -p 4000 -c $ROBOTS_CONFIG_DIR -r $NAME -s &
     $NODE_INTERPRETER $WEBUI_SERVER -p 8000 -c $ROBOTS_CONFIG_DIR -r $NAME; $SHELL"
 
-tmux new-window -n 'blender' "cd $HR_WORKSPACE/HEAD/src/blender_api && blender -y $BLENDER_FILE -P autostart.py; $SHELL"
+tmux new-window -n 'blender' "export PYTHONPATH=$PYTHONPATH:$PY3PATH && cd $HR_PREFIX/tools/blender && blender -y $BLENDER_FILE -P autostart.py; $SHELL"
 
 if [[ -f $HR_PREFIX/bin/run_tts_server ]]; then
-    tmux new-window -n 'tts' "$HR_PREFIX/bin/run_tts_server --voice_path=$HR_WORKSPACE/HEAD/src/tts/api; $SHELL"
+    tmux new-window -n 'servers' "TTSSERVER=$HR_PREFIX/bin/run_tts_server supervisord -n -c $BASEDIR/supervisord.conf; $SHELL"
 else
     error "ttsserver is not installed. Run 'hr install head-python-ttsserver'"
 fi
 
 if [[ -f $HR_PREFIX/ros/lib/chatbot/run_server.py ]]; then
-    tmux new-window -n 'chatbot' "$HR_PREFIX/ros/lib/chatbot/run_server.py; $SHELL"
+    tmux new-window -n 'chatbot' "bash -c \"$HR_PREFIX/py2env/bin/python $HR_PREFIX/ros/lib/chatbot/run_server.py\"; $SHELL"
 else
     error "chatbot is not installed. Run 'hr install head-chatbot'"
 fi
@@ -289,4 +348,4 @@ if [[ $DEV_MODE == 1 ]]; then
     rosrun dynamic_reconfigure dynparam set /${NAME}/tts_talker lipsync_blender true
 fi
 
-tmux attach;
+info "Processes are running background. To attach to this session, run 'tmux a'"
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,21 +1,32 @@
 #!/usr/bin/env bash
-#
-# dev.sh
-#
-# This script stops running software.
-#
-if [ -z "$1" ]; then
-    if [ -z "$NAME" ]; then
-        NAME=robot;
-    fi
-else
-    NAME=$1
+
+BASEDIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+source $BASEDIR/common.sh
+PIDFILE=/tmp/hr.pid
+
+if [[ -f $PIDFILE ]]; then
+    pids=$(<"$PIDFILE")
+    kill $pids
+    rm $PIDFILE
 fi
 
-SESSION_INFO=$(tmux ls 2>/dev/null | grep $NAME)
-if [[ ${SESSION_INFO} == ${NAME}* ]]; then
-    curl -G localhost:8001/v1.1/dump_history
-    tmux kill-session -t $NAME
+ns=$NAME
+
+supervisorctl -c $BASEDIR/supervisord.conf shutdown >/dev/null
+sleep 0.5
+
+if [[ $(tmux ls 2>/dev/null) ]]; then
+    # TODO: don't send command when stopping pololu
+    # cd $HR_WORKSPACE/HEAD/src/tools/src/testing_tools && python live_off.py && cd -
+    # rostopic pub -1 /behavior_switch std_msgs/String "btree_off"
+    # rosservice call /${ns}/head_pau_mux/select /${ns}/no_pau
+    # rosservice call /${ns}/neck_pau_mux/select /${ns}/cmd_neck_pau
+    # rostopic pub -1 /${ns}/point_head basic_head_api/PointHead 0 0 0
+    # rostopic pub -1 /${ns}/make_face_expr basic_head_api/MakeFaceExpr Neutral 0
+    curl -G localhost:8001/v1.1/dump_history >/dev/null 2>&1
+    # bash $HR_WORKSPACE/hr-solr/stop.sh
+    tmux kill-session
 fi
 
-echo "Stopped"
+check_ports 1024 4000 8000 8001 9090 10001
+info "Stopped $?"
